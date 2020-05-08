bc7c991ddea848c3071e8931fb8597dca9f3463c
Matti Kortelainen <matti.kortelainen@cern.ch>
2020-05-08 14:18:58 -0500

    [kokkostest] Initialize only the backends actually used

--- src/kokkostest/KokkosCore/kokkoshost/kokkosConfigCommon.cc
+++ src/kokkostest/KokkosCore/kokkoshost/kokkosConfigCommon.cc
@@ -5,16 +5,26 @@
 namespace kokkos_common {
   class InitializeScopeGuard::Impl {
   public:
-    explicit Impl(const Kokkos::InitArguments& args) : guard(args) {}
+    explicit Impl(std::vector<Backend> const& backends, Kokkos::InitArguments const& args) {
+      Kokkos::Impl::pre_initialize(args);
+      if (std::find(backends.begin(), backends.end(), Backend::SERIAL) != backends.end()) {
+        Kokkos::Serial::impl_initialize();
+      }
+      if (std::find(backends.begin(), backends.end(), Backend::CUDA) != backends.end()) {
+        // CUDA execution space requires a host execution space as well
+        Kokkos::Serial::impl_initialize();
+        Kokkos::Cuda::impl_initialize();
+      }
+      Kokkos::Impl::post_initialize(args);
+    }
 
-  private:
-    Kokkos::ScopeGuard guard;
+    ~Impl() { Kokkos::finalize(); }
   };
 
-  InitializeScopeGuard::InitializeScopeGuard() {
+  InitializeScopeGuard::InitializeScopeGuard(std::vector<Backend> const& backends) {
     // for now pass in the default arguments
     Kokkos::InitArguments arguments;
-    pimpl_ = std::make_unique<Impl>(arguments);
+    pimpl_ = std::make_unique<Impl>(backends, arguments);
   }
 
   InitializeScopeGuard::~InitializeScopeGuard() = default;
