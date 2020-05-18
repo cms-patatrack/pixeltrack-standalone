6409c08159f98e6f75cf4c3a6a57f0428268747c
Matti Kortelainen <matti.kortelainen@cern.ch>
2020-05-18 03:44:49 +0200

    [cudauvm] Fork cuda to cudauvm

---
+++ src/cudauvm/plugin-PixelTriplets/CAHitNtupletGeneratorOnGPU.cc
@@ -0,0 +1,167 @@
+//
+// Original Author: Felice Pantaleo, CERN
+//
+
+#include <array>
+#include <cassert>
+#include <functional>
+#include <vector>
+
+#include "Framework/Event.h"
+
+#include "CAHitNtupletGeneratorOnGPU.h"
+
+namespace {
+
+  template <typename T>
+  T sqr(T x) {
+    return x * x;
+  }
+
+  cAHitNtupletGenerator::QualityCuts makeQualityCuts() {
+    auto coeff = std::vector<double>{0.68177776, 0.74609577, -0.08035491, 0.00315399};  // chi2Coeff
+    return cAHitNtupletGenerator::QualityCuts{// polynomial coefficients for the pT-dependent chi2 cut
+                                              {(float)coeff[0], (float)coeff[1], (float)coeff[2], (float)coeff[3]},
+                                              // max pT used to determine the chi2 cut
+                                              10.f,  // chi2MaxPt
+                                                     // chi2 scale factor: 30 for broken line fit, 45 for Riemann fit
+                                              30.f,  // chi2Scale
+                                                     // regional cuts for triplets
+                                              {
+                                                  0.3f,  //tripletMaxTip
+                                                  0.5f,  // tripletMinPt
+                                                  12.f   // tripletMaxZip
+                                              },
+                                              // regional cuts for quadruplets
+                                              {
+                                                  0.5f,  // quadrupletMaxTip
+                                                  0.3f,  // quadrupletMinPt
+                                                  12.f   // quadrupletMaxZip
+                                              }};
+  }
+}  // namespace
+
+using namespace std;
+CAHitNtupletGeneratorOnGPU::CAHitNtupletGeneratorOnGPU(edm::ProductRegistry& reg)
+    : m_params(true,              // onGPU
+               3,                 // minHitsPerNtuplet,
+               458752,            // maxNumberOfDoublets
+               false,             //useRiemannFit
+               true,              // fit5as4,
+               true,              //includeJumpingForwardDoublets
+               true,              // earlyFishbone
+               false,             // lateFishbone
+               true,              // idealConditions
+               false,             //fillStatistics
+               true,              // doClusterCut
+               true,              // doZ0Cut
+               true,              // doPtCut
+               0.899999976158,    // ptmin
+               0.00200000009499,  // CAThetaCutBarrel
+               0.00300000002608,  // CAThetaCutForward
+               0.0328407224959,   // hardCurvCut
+               0.15000000596,     // dcaCutInnerTriplet
+               0.25,              // dcaCutOuterTriplet
+               makeQualityCuts()) {
+#ifdef DUMP_GPU_TK_TUPLES
+  printf("TK: %s %s % %s %s %s %s %s %s %s %s %s %s %s %s %s\n",
+         "tid",
+         "qual",
+         "nh",
+         "charge",
+         "pt",
+         "eta",
+         "phi",
+         "tip",
+         "zip",
+         "chi2",
+         "h1",
+         "h2",
+         "h3",
+         "h4",
+         "h5");
+#endif
+
+  if (m_params.onGPU_) {
+    cudaCheck(cudaMalloc(&m_counters, sizeof(Counters)));
+    cudaCheck(cudaMemset(m_counters, 0, sizeof(Counters)));
+  } else {
+    m_counters = new Counters();
+    memset(m_counters, 0, sizeof(Counters));
+  }
+}
+
+CAHitNtupletGeneratorOnGPU::~CAHitNtupletGeneratorOnGPU() {
+  if (m_params.doStats_) {
+    // crash on multi-gpu processes
+    if (m_params.onGPU_) {
+      CAHitNtupletGeneratorKernelsGPU::printCounters(m_counters);
+    } else {
+      CAHitNtupletGeneratorKernelsCPU::printCounters(m_counters);
+    }
+  }
+  if (m_params.onGPU_) {
+    cudaFree(m_counters);
+  } else {
+    delete m_counters;
+  }
+}
+
+PixelTrackHeterogeneous CAHitNtupletGeneratorOnGPU::makeTuplesAsync(TrackingRecHit2DCUDA const& hits_d,
+                                                                    float bfield,
+                                                                    cudaStream_t stream) const {
+  PixelTrackHeterogeneous tracks(cms::cuda::make_device_unique<pixelTrack::TrackSoA>(stream));
+
+  auto* soa = tracks.get();
+
+  CAHitNtupletGeneratorKernelsGPU kernels(m_params);
+  kernels.counters_ = m_counters;
+  HelixFitOnGPU fitter(bfield, m_params.fit5as4_);
+
+  kernels.allocateOnGPU(stream);
+  fitter.allocateOnGPU(&(soa->hitIndices), kernels.tupleMultiplicity(), soa);
+
+  kernels.buildDoublets(hits_d, stream);
+  kernels.launchKernels(hits_d, soa, stream);
+  kernels.fillHitDetIndices(hits_d.view(), soa, stream);  // in principle needed only if Hits not "available"
+  if (m_params.useRiemannFit_) {
+    fitter.launchRiemannKernels(hits_d.view(), hits_d.nHits(), CAConstants::maxNumberOfQuadruplets(), stream);
+  } else {
+    fitter.launchBrokenLineKernels(hits_d.view(), hits_d.nHits(), CAConstants::maxNumberOfQuadruplets(), stream);
+  }
+  kernels.classifyTuples(hits_d, soa, stream);
+
+  return tracks;
+}
+
+PixelTrackHeterogeneous CAHitNtupletGeneratorOnGPU::makeTuples(TrackingRecHit2DCPU const& hits_d, float bfield) const {
+  PixelTrackHeterogeneous tracks(std::make_unique<pixelTrack::TrackSoA>());
+
+  auto* soa = tracks.get();
+  assert(soa);
+
+  CAHitNtupletGeneratorKernelsCPU kernels(m_params);
+  kernels.counters_ = m_counters;
+  kernels.allocateOnGPU(nullptr);
+
+  kernels.buildDoublets(hits_d, nullptr);
+  kernels.launchKernels(hits_d, soa, nullptr);
+  kernels.fillHitDetIndices(hits_d.view(), soa, nullptr);  // in principle needed only if Hits not "available"
+
+  if (0 == hits_d.nHits())
+    return tracks;
+
+  // now fit
+  HelixFitOnGPU fitter(bfield, m_params.fit5as4_);
+  fitter.allocateOnGPU(&(soa->hitIndices), kernels.tupleMultiplicity(), soa);
+
+  if (m_params.useRiemannFit_) {
+    fitter.launchRiemannKernelsOnCPU(hits_d.view(), hits_d.nHits(), CAConstants::maxNumberOfQuadruplets());
+  } else {
+    fitter.launchBrokenLineKernelsOnCPU(hits_d.view(), hits_d.nHits(), CAConstants::maxNumberOfQuadruplets());
+  }
+
+  kernels.classifyTuples(hits_d, soa, nullptr);
+
+  return tracks;
+}
