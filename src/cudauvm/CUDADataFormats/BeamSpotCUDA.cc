e3d2f8dacfcfd34322403c384bd7509c76dbe485
Matti Kortelainen <matti.kortelainen@cern.ch>
2020-05-21 04:07:09 +0200

    [cudauvm] Migrate BeamSpot transfer to managed memory

--- src/cudauvm/CUDADataFormats/BeamSpotCUDA.cc
+++ src/cudauvm/CUDADataFormats/BeamSpotCUDA.cc
@@ -1,9 +1,15 @@
 #include "CUDADataFormats/BeamSpotCUDA.h"
 
 #include "CUDACore/cudaCheck.h"
-#include "CUDACore/device_unique_ptr.h"
+#include "CUDACore/managed_unique_ptr.h"
 
-BeamSpotCUDA::BeamSpotCUDA(Data const* data_h, cudaStream_t stream) {
-  data_d_ = cms::cuda::make_device_unique<Data>(stream);
-  cudaCheck(cudaMemcpyAsync(data_d_.get(), data_h, sizeof(Data), cudaMemcpyHostToDevice, stream));
+BeamSpotCUDA::BeamSpotCUDA(Data const& data_h, int device, cudaStream_t stream) {
+  data_d_ = cms::cuda::make_managed_unique<Data>(stream);
+  *data_d_ = data_h;
+#ifndef CUDAUVM_DISABLE_ADVICE
+  cudaCheck(cudaMemAdvise(data_d_.get(), sizeof(Data), cudaMemAdviseSetReadMostly, device));
+#endif
+#ifndef CUDAUVM_DISABLE_PREFETCH
+  cudaCheck(cudaMemPrefetchAsync(data_d_.get(), sizeof(Data), device, stream));
+#endif
 }
