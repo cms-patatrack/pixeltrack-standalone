388a88d6427fa6563566e510126ad5cc2067986d
Matti Kortelainen <matti.kortelainen@cern.ch>
2020-05-21 20:21:09 -0500

    [cudauvm] Move SiPixelFedCalingMapGPUWrapper to managed memory

--- src/cudauvm/CondFormats/SiPixelFedCablingMapGPUWrapper.h
+++ src/cudauvm/CondFormats/SiPixelFedCablingMapGPUWrapper.h
@@ -2,8 +2,6 @@
 #define RecoLocalTracker_SiPixelClusterizer_SiPixelFedCablingMapGPUWrapper_h
 
 #include "CUDACore/ESProduct.h"
-#include "CUDACore/CUDAHostAllocator.h"
-#include "CUDACore/device_unique_ptr.h"
 #include "CondFormats/SiPixelFedCablingMapGPU.h"
 
 #include <cuda_runtime.h>
@@ -12,35 +10,23 @@
 
 class SiPixelFedCablingMapGPUWrapper {
 public:
-  explicit SiPixelFedCablingMapGPUWrapper(SiPixelFedCablingMapGPU const &cablingMap,
-                                          std::vector<unsigned char> modToUnp);
+  explicit SiPixelFedCablingMapGPUWrapper(SiPixelFedCablingMapGPU const& cablingMap,
+                                          std::vector<unsigned char> const& modToUnp);
   ~SiPixelFedCablingMapGPUWrapper();
 
   bool hasQuality() const { return hasQuality_; }
 
   // returns pointer to GPU memory
-  const SiPixelFedCablingMapGPU *getGPUProductAsync(cudaStream_t cudaStream) const;
+  const SiPixelFedCablingMapGPU* cablingMap() const { return cablingMap_; }
 
   // returns pointer to GPU memory
-  const unsigned char *getModToUnpAllAsync(cudaStream_t cudaStream) const;
+  const unsigned char* modToUnpAll() const { return modToUnpDefault_; }
 
 private:
-  std::vector<unsigned char, CUDAHostAllocator<unsigned char>> modToUnpDefault;
   bool hasQuality_;
 
-  SiPixelFedCablingMapGPU *cablingMapHost = nullptr;  // pointer to struct in CPU
-
-  struct GPUData {
-    ~GPUData();
-    SiPixelFedCablingMapGPU *cablingMapDevice = nullptr;  // pointer to struct in GPU
-  };
-  cms::cuda::ESProduct<GPUData> gpuData_;
-
-  struct ModulesToUnpack {
-    ~ModulesToUnpack();
-    unsigned char *modToUnpDefault = nullptr;  // pointer to GPU
-  };
-  cms::cuda::ESProduct<ModulesToUnpack> modToUnp_;
+  SiPixelFedCablingMapGPU* cablingMap_ = nullptr;
+  unsigned char* modToUnpDefault_ = nullptr;
 };
 
 #endif
