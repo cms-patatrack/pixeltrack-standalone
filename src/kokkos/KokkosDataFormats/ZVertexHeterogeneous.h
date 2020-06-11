66ddc76c33c2eb090d85b42d54457cc58d1eeddb
Yunsong Wang <yunsongwang@lbl.gov>
2020-06-11 16:47:57 -0700

    [kokkos] Add ZVertexHeterogeneous header file

---
+++ src/kokkos/KokkosDataFormats/ZVertexHeterogeneous.h
@@ -0,0 +1,13 @@
+#ifndef KokkosDataFormatsVertexZVertexHeterogeneous_H
+#define KokkosDataFormatsVertexZVertexHeterogeneous_H
+
+#include "DataFormats/ZVertexSoA.h"
+//#include "KokkosDataFormats/HeterogeneousSoA.h"
+
+//using ZVertexHeterogeneous = HeterogeneousSoA<ZVertexSoA>;
+//#ifndef __CUDACC__
+//#include "CUDACore/Product.h"
+//using ZVertexCUDAProduct = cms::cuda::Product<ZVertexHeterogeneous>;
+//#endif
+
+#endif
