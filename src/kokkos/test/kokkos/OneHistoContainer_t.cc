aa82530be01e8e48f05d75c6d1066f05a03f4215
Taylor Childers <taylor@jtchilders.com>
2020-06-18 09:44:49 -0500

    completed conversion to kokkos; needed to change the logic quite a bit; no more big loop for all threads; multiple smaller parallel kokkos calls

--- src/kokkos/test/kokkos/OneHistoContainer_t.cc
+++ src/kokkos/test/kokkos/OneHistoContainer_t.cc
@@ -21,9 +21,10 @@ void go() {
     rmax = NBINS * 2 - 1;
   }
 
+  std::default_random_engine generator (1234);
   std::uniform_int_distribution<T> rgen(rmin, rmax);
 
-  constexpr int N = 12000;
+  constexpr uint32_t N = 20000;
   Kokkos::View<T*,KokkosExecSpace> v_d("v_d",N);
   typename Kokkos::View<T*,KokkosExecSpace>::HostMirror v_h("v_h",N);
 
@@ -32,7 +33,8 @@ void go() {
             << (rmax - rmin) / Hist::nbins() << std::endl;
   std::cout << "bins " << int(Hist::bin(0)) << ' ' << int(Hist::bin(rmin)) << ' ' << int(Hist::bin(rmax)) << std::endl;
 
-  for (int it = 0; it < 5; ++it) {
+  for (int it = 0; it < 1; ++it) {
+    // printf("it = %2i\n",it);
     for (long long j = 0; j < N; j++)
       v_h(j) = rgen(eng);
     if (it == 2)
@@ -40,85 +42,75 @@ void go() {
         v_h(j) = 4;
 
     Kokkos::deep_copy(v_d,v_h);
-    team_policy policy(1,256);
-    int level = 0;
-
+    
     using TeamHist = HistoContainer<T, NBINS, N, S, uint16_t>;
-    using TeamHistView = Kokkos::View<TeamHist*,KokkosExecSpace::scratch_memory_space,Kokkos::MemoryUnmanaged>;
-    size_t team_hist_bytes = TeamHistView::shmem_size(1);
 
-    using WSView = typename Kokkos::View<typename TeamHist::Counter*,KokkosExecSpace::scratch_memory_space,Kokkos::MemoryUnmanaged>;
-    int ws_size = 32;
-    size_t team_ws_bytes = WSView::shmem_size(ws_size);
-    printf("starting loop: creating shared mem size: %10lu\n",team_hist_bytes+team_ws_bytes);
-    Kokkos::parallel_for("mykernel",policy.set_scratch_size(level,Kokkos::PerTeam(team_hist_bytes+team_ws_bytes)),
-                         KOKKOS_LAMBDA(const member_type& teamMember){
+    Kokkos::View<TeamHist*,KokkosExecSpace> histo_d("histo_d",1);
+    auto histo_h = Kokkos::create_mirror_view(histo_d);
 
-      uint32_t threadId = teamMember.league_rank() * teamMember.team_size() + teamMember.team_rank();
-      printf("threadId = %10u",threadId);
-      if (threadId == 0)
-        printf("start kernel for %d data\n", N);
-
-      TeamHistView shared_histo(teamMember.team_scratch(level),1);
-      auto hist = shared_histo(0);
-      WSView ws(teamMember.team_scratch(level),ws_size);
-
-//      for (auto j = threadId; j < TeamHist::totbins(); j += teamMember.team_size()) {
-      Kokkos::parallel_for(Kokkos::TeamThreadRange(teamMember,TeamHist::totbins()),
-         [&](const int& j){
-        hist.off[j] = 0;
+    Kokkos::parallel_for("set_zero",Kokkos::RangePolicy<KokkosExecSpace>(0,TeamHist::totbins()),
+      KOKKOS_LAMBDA(const int& i){
+        histo_d(0).off[i] = 0;
       });
-      teamMember.team_barrier();
-      Kokkos::single(Kokkos::PerTeam(teamMember), [&]{
-        for(uint32_t i = 0;i<TeamHist::totbins();++i){
-          printf("hist.off[%02u] = %04u\n",i,hist.off[i]);
-        }
+
+    Kokkos::parallel_for("set_zero_bin",Kokkos::RangePolicy<KokkosExecSpace>(0,TeamHist::capacity()),
+      KOKKOS_LAMBDA(const int& i){
+        histo_d(0).bins[i] = 0;
       });
-#ifdef TODO
-      for (auto j = threadId; j < N; j += teamMember.team_size())
-        hist.count(v_d(j));
-      teamMember.team_barrier();
 
-      assert(0 == hist.size());
-      teamMember.team_barrier();
+    Kokkos::parallel_for("count",Kokkos::RangePolicy<KokkosExecSpace>(0,N),
+      KOKKOS_LAMBDA(const int& i){
+        histo_d(0).count(v_d(i));
+      });
 
-      hist.finalize(&ws(0),teamMember);
-      teamMember.team_barrier();
+    Kokkos::deep_copy(histo_h,histo_d);
+    assert(histo_h(0).off[-1] == 0);
+    assert(0 == histo_h(0).size());
 
-      assert(N == hist.size());
-      for (auto j = threadId; j < Hist::nbins(); j += teamMember.team_size())
-        assert(hist.off[j] <= hist.off[j + 1]);
-      teamMember.team_barrier();
+    TeamHist::finalize(histo_d);
 
-      if (threadId < 32)
-        ws[threadId] = 0;  // used by prefix scan...
-      teamMember.team_barrier();
+    Kokkos::deep_copy(histo_h,histo_d);
 
-      for (auto j = threadIdx.x; j < N; j += blockDim.x)
-        hist.fill(v_d(j), j);
-      teamMember.team_barrier();
-      assert(0 == hist.off[0]);
-      assert(N == hist.size());
+    assert(histo_h(0).off[-1] == histo_h(0).off[-2]);
+    assert(N == histo_h(0).size());
 
-      for (auto j = threadId; j < hist.size() - 1; j += teamMember.team_size()) {
-        auto p = hist.begin() + j;
-        assert((*p) < N);
+    Kokkos::parallel_for("assert_check",Kokkos::RangePolicy<KokkosExecSpace>(0,TeamHist::totbins()-1),
+      KOKKOS_LAMBDA(const int& i){
+        assert(histo_d(0).off[i] <= histo_d(0).off[i+1]);
+      });
+
+    Kokkos::parallel_for("fill",Kokkos::RangePolicy<KokkosExecSpace>(0,N),
+      KOKKOS_LAMBDA(const int& i){
+        histo_d(0).fill(v_d(i),i);
+      });
+
+    Kokkos::deep_copy(histo_h,histo_d);
+
+    assert(0 == histo_h(0).off[0]);
+    assert(N == histo_h(0).size());
+
+    Kokkos::parallel_for("bin",Kokkos::RangePolicy<KokkosExecSpace>(0,N-1),
+      KOKKOS_LAMBDA(const int& i){
+        auto p = histo_d(0).begin() + i;
+        assert( (*p) < N);
         auto k1 = TeamHist::bin(v_d(*p));
-        auto k2 = TeamHist::bin(v_d(*(p + 1)));
+        auto k2 = TeamHist::bin(v_d(*(p+1)));
         assert(k2 >= k1);
-      }
+      });
 
-      for (auto i = threadId; i < hist.size(); i += teamMember.team_size()) {
-        auto p = hist.begin() + i;
+    Kokkos::parallel_for("forEachInWindow",Kokkos::RangePolicy<KokkosExecSpace>(0,N),
+      KOKKOS_LAMBDA(const int& i){
+        auto p = histo_d(0).begin() + i;
         auto j = *p;
         auto b0 = TeamHist::bin(v_d(j));
         int tot = 0;
-        auto ftest = [&](int k) {
+        auto ftest = [&](int k){
           assert(k >= 0 && k < N);
           ++tot;
         };
-        forEachInWindow(hist, v_d(j), v_d(j), ftest);
-        int rtot = hist.size(b0);
+        forEachInWindow(histo_d,v_d(j), v_d(j), ftest);
+        int rtot = histo_d(0).size(b0);
+
         assert(tot == rtot);
         tot = 0;
         auto vm = int(v_d(j)) - DELTA;
@@ -129,23 +121,24 @@ void go() {
         vp = std::min(vp, vmax);
         vp = std::max(vp, 0);
         assert(vp >= vm);
-        forEachInWindow(hist, vm, vp, ftest);
+        forEachInWindow(histo_d, vm, vp, ftest);
         int bp = TeamHist::bin(vp);
         int bm = TeamHist::bin(vm);
-        rtot = hist.end(bp) - hist.begin(bm);
+        rtot = histo_d(0).end(bp) - histo_d(0).begin(bm);
         assert(tot == rtot);
-      }
-#endif // TODO
 
-    }); // KOKKOS_LAMBDA
+
+      });
+
   }
 }
 
 int main() {
-  kokkos_common::InitializeScopeGuard kokkosGuard;
+  kokkos_common::InitializeScopeGuard kokkosGuard({KokkosBackend<KokkosExecSpace>::value});
+  // cudaDeviceSetLimit(cudaLimitPrintfFifoSize,1024*1024*1024);
   go<int16_t>();
-//  go<uint8_t, 128, 8, 4>();
-//  go<uint16_t, 313 / 2, 9, 4>();
+  go<uint8_t, 128, 8, 4>();
+  go<uint16_t, 313 / 2, 9, 4>();
 
   return 0;
 }
