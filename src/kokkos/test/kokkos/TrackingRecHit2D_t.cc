1418089732503f683dc6a72360c4646959905e4b
Matti Kortelainen <matti.kortelainen@cern.ch>
2020-09-15 13:32:30 -0700

    code format

--- src/kokkos/test/kokkos/TrackingRecHit2D_t.cc
+++ src/kokkos/test/kokkos/TrackingRecHit2D_t.cc
@@ -6,64 +6,64 @@
 
 namespace testTrackingRecHit2DKokkos {
 
-  template<typename MemorySpace>
+  template <typename MemorySpace>
   void fill(Kokkos::View<TrackingRecHit2DSOAView, MemorySpace> hits) {
-
     assert(hits.data());
     auto hits_ = &hits();
 
-    Kokkos::parallel_for("fill", Kokkos::RangePolicy<KokkosExecSpace>(KokkosExecSpace(), 0, 1024), KOKKOS_LAMBDA(const size_t i) {
-              assert(hits_->nHits() == 200);
-              if (i > 200) return;
-            });
+    Kokkos::parallel_for(
+        "fill", Kokkos::RangePolicy<KokkosExecSpace>(KokkosExecSpace(), 0, 1024), KOKKOS_LAMBDA(const size_t i) {
+          assert(hits_->nHits() == 200);
+          if (i > 200)
+            return;
+        });
 
     return;
   }
 
-  template<typename MemorySpace>
+  template <typename MemorySpace>
   void verify(Kokkos::View<TrackingRecHit2DSOAView, MemorySpace> hits) {
-
     assert(hits.data());
 
     auto const hits_ = &hits();
 
-    Kokkos::parallel_for("fill", Kokkos::RangePolicy<KokkosExecSpace>(0, 1024), KOKKOS_LAMBDA(const size_t i) {
-              assert(hits_->nHits() == 200);
-              if (i > 200) return;
-            });
+    Kokkos::parallel_for(
+        "fill", Kokkos::RangePolicy<KokkosExecSpace>(0, 1024), KOKKOS_LAMBDA(const size_t i) {
+          assert(hits_->nHits() == 200);
+          if (i > 200)
+            return;
+        });
 
     return;
   }
 
-  template<typename MemorySpace>
+  template <typename MemorySpace>
   void runKernels(Kokkos::View<TrackingRecHit2DSOAView, MemorySpace> hits) {
     assert(hits.data());
 
     fill(hits);
-    verify(hits);    
+    verify(hits);
   }
 }  // namespace testTrackingRecHit2DKokkos
 
 namespace testTrackingRecHit2DKokkos {
-  template<typename MemorySpace>  
+  template <typename MemorySpace>
   void runKernels(Kokkos::View<TrackingRecHit2DSOAView, MemorySpace> hits);
 
 }
 
-int main() {	
-
+int main() {
   kokkos_common::InitializeScopeGuard kokkosGuard({KokkosBackend<KokkosExecSpace>::value});
 
   {
     auto nHits = 200;
 
     Kokkos::View<pixelCPEforGPU::ParamsOnGPU, KokkosExecSpace> _cpeParams("cpeparams");
-    Kokkos::View<uint32_t*, KokkosExecSpace> _hitsModuleStart("hitsmodulestart",1);
+    Kokkos::View<uint32_t*, KokkosExecSpace> _hitsModuleStart("hitsmodulestart", 1);
 
     TrackingRecHit2DKokkos<KokkosExecSpace> tkhit(nHits, _cpeParams, _hitsModuleStart, KokkosExecSpace());
 
     testTrackingRecHit2DKokkos::runKernels<KokkosExecSpace>(tkhit.mView());
-
   }
 
   return 0;
