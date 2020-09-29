922569dacdbcfaa819f8deb9aeb5f3145a4c78e0
Andrea Bocci <andrea.bocci@cern.ch>
2020-09-29 12:17:07 +0200

    [sycl] Round robin through all SYCL devices

--- src/sycltest/CUDACore/chooseDevice.cc
+++ src/sycltest/CUDACore/chooseDevice.cc
@@ -1,14 +1,19 @@
+#include <vector>
+
+#include <CL/sycl.hpp>
+
 #include "chooseDevice.h"
-#include "deviceCount.h"
 
 namespace cms::cuda {
-  int chooseDevice(edm::StreamID id) {
+  sycl::device chooseDevice(edm::StreamID id) {
     // For startes we "statically" assign the device based on
     // edm::Stream number. This is suboptimal if the number of
     // edm::Streams is not a multiple of the number of CUDA devices
     // (and even then there is no load balancing).
     //
     // TODO: improve the "assignment" logic
-    return id % deviceCount();
+    static std::vector<sycl::device> devices = sycl::device::get_devices(sycl::info::device_type::all);
+
+    return devices[id % devices.size()];
   }
 }  // namespace cms::cuda
--- src/sycltest/CUDACore/chooseDevice.h
+++ src/sycltest/CUDACore/chooseDevice.h
@@ -1,10 +1,12 @@
 #ifndef HeterogeneousCore_CUDACore_chooseDevice_h
 #define HeterogeneousCore_CUDACore_chooseDevice_h
 
+#include <CL/sycl.hpp>
+
 #include "Framework/Event.h"
 
 namespace cms::cuda {
-  int chooseDevice(edm::StreamID id);
+  sycl::device chooseDevice(edm::StreamID id);
 }
 
 #endif
