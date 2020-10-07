6062a5900b63381f10acc14a2a60945149d3bb96
Andrea Bocci <andrea.bocci@cern.ch>
2020-10-07 10:55:01 +0200

    [sycl] Simplify the use of sycl::nd_range

--- src/sycltest/plugin-Test2/gpuAlgo2.cc
+++ src/sycltest/plugin-Test2/gpuAlgo2.cc
@@ -11,17 +11,17 @@ namespace gpu_algo_2 {
   constexpr int NUM_VALUES = 1000;
 
   template <typename T>
-  SYCL_EXTERNAL void vectorAdd(const T *a, const T *b, T *c, int numElements, sycl::nd_item<3> item_ct1) {
-    int i = item_ct1.get_group(2) * item_ct1.get_local_range().get(2) + item_ct1.get_local_id(2);
+  SYCL_EXTERNAL void vectorAdd(const T *a, const T *b, T *c, int numElements, sycl::nd_item<1> item) {
+    int i = item.get_group(0) * item.get_local_range().get(0) + item.get_local_id(0);
     if (i < numElements) {
       c[i] = a[i] + b[i];
     }
   }
 
   template <typename T>
-  SYCL_EXTERNAL void vectorProd(const T *a, const T *b, T *c, int numElements, sycl::nd_item<3> item_ct1) {
-    int row = item_ct1.get_group(1) * item_ct1.get_local_range().get(1) + item_ct1.get_local_id(1);
-    int col = item_ct1.get_group(2) * item_ct1.get_local_range().get(2) + item_ct1.get_local_id(2);
+  SYCL_EXTERNAL void vectorProd(const T *a, const T *b, T *c, int numElements, sycl::nd_item<3> item) {
+    int row = item.get_group(1) * item.get_local_range().get(1) + item.get_local_id(1);
+    int col = item.get_group(2) * item.get_local_range().get(2) + item.get_local_id(2);
 
     if (row < numElements && col < numElements) {
       c[row * numElements + col] = a[row] * b[col];
@@ -29,9 +29,9 @@ namespace gpu_algo_2 {
   }
 
   template <typename T>
-  SYCL_EXTERNAL void matrixMul(const T *a, const T *b, T *c, int numElements, sycl::nd_item<3> item_ct1) {
-    int row = item_ct1.get_group(1) * item_ct1.get_local_range().get(1) + item_ct1.get_local_id(1);
-    int col = item_ct1.get_group(2) * item_ct1.get_local_range().get(2) + item_ct1.get_local_id(2);
+  SYCL_EXTERNAL void matrixMul(const T *a, const T *b, T *c, int numElements, sycl::nd_item<3> item) {
+    int row = item.get_group(1) * item.get_local_range().get(1) + item.get_local_id(1);
+    int col = item.get_group(2) * item.get_local_range().get(2) + item.get_local_id(2);
 
     if (row < numElements && col < numElements) {
       T tmp = 0;
@@ -43,8 +43,8 @@ namespace gpu_algo_2 {
   }
 
   template <typename T>
-  SYCL_EXTERNAL void matrixMulVector(const T *a, const T *b, T *c, int numElements, sycl::nd_item<3> item_ct1) {
-    int row = item_ct1.get_group(1) * item_ct1.get_local_range().get(1) + item_ct1.get_local_id(1);
+  SYCL_EXTERNAL void matrixMulVector(const T *a, const T *b, T *c, int numElements, sycl::nd_item<1> item) {
+    int row = item.get_group(1) * item.get_local_range().get(1) + item.get_local_id(1);
 
     if (row < numElements) {
       T tmp = 0;
@@ -54,7 +54,7 @@ namespace gpu_algo_2 {
       c[row] = tmp;
     }
   }
-}  // namespace
+}  // namespace gpu_algo_2
 
 using namespace gpu_algo_2;
 
@@ -91,84 +91,65 @@ cms::sycltools::device::unique_ptr<float[]> gpuAlgo2(sycl::queue stream) {
 
   unsigned int threadsPerBlock = std::min<unsigned int>(NUM_VALUES, workgroupSize);
   unsigned int blocksPerGrid = (NUM_VALUES + threadsPerBlock - 1) / threadsPerBlock;
+  unsigned int threadsPerGrid = threadsPerGrid;
 
-  std::cerr << "block size: " << threadsPerBlock << " / " << stream.get_device().get_info<sycl::info::device::max_work_group_size>() << std::endl; 
-  std::cerr << "grid size:  " << blocksPerGrid   << std::endl;
+  std::cerr << "block size: " << threadsPerBlock << " / "
+            << stream.get_device().get_info<sycl::info::device::max_work_group_size>() << std::endl;
+  std::cerr << "grid size:  " << blocksPerGrid << std::endl;
 
   auto d_c = cms::sycltools::make_device_unique<float[]>(NUM_VALUES, stream);
   stream.submit([&](sycl::handler &cgh) {
-    auto d_a_get_ct0 = d_a.get();
-    auto d_b_get_ct1 = d_b.get();
-    auto d_c_get_ct2 = d_c.get();
+    auto pd_a = d_a.get();
+    auto pd_b = d_b.get();
+    auto pd_c = d_c.get();
 
-    cgh.parallel_for(sycl::nd_range<3>(sycl::range<3>(1, 1, blocksPerGrid) * sycl::range<3>(1, 1, threadsPerBlock),
-                                       sycl::range<3>(1, 1, threadsPerBlock)),
-                     [=](sycl::nd_item<3> item_ct1) {
-                       vectorAdd(d_a_get_ct0, d_b_get_ct1, d_c_get_ct2, NUM_VALUES, item_ct1);
-                     });
+    cgh.parallel_for(sycl::nd_range<1>(threadsPerGrid, threadsPerBlock),
+                     [=](sycl::nd_item<1> item) { vectorAdd(pd_a, pd_b, pd_c, NUM_VALUES, item); });
   });
 
   auto d_ma = cms::sycltools::make_device_unique<float[]>(NUM_VALUES * NUM_VALUES, stream);
   auto d_mb = cms::sycltools::make_device_unique<float[]>(NUM_VALUES * NUM_VALUES, stream);
   auto d_mc = cms::sycltools::make_device_unique<float[]>(NUM_VALUES * NUM_VALUES, stream);
 
-  sycl::range<3> threadsPerBlock3{workgroupRectA, workgroupRectB, 1};
-  sycl::range<3> blocksPerGrid3{(NUM_VALUES + threadsPerBlock3[0] - 1) / threadsPerBlock3[0], (NUM_VALUES + threadsPerBlock3[1] - 1) / threadsPerBlock3[1], 1};
+  sycl::range<3> threadsPerBlock3{1, workgroupRectB, workgroupRectA};
+  sycl::range<3> blocksPerGrid3{
+      1, (NUM_VALUES + workgroupRectB - 1) / workgroupRectB, (NUM_VALUES + workgroupRectA - 1) / workgroupRectA};
+  auto threadsPerGrid3 = blocksPerGrid3 * threadsPerBlock3;
+
   stream.submit([&](sycl::handler &cgh) {
-    auto threadsPerGrid = blocksPerGrid3 * threadsPerBlock3;
+    auto pd_a = d_a.get();
+    auto pd_b = d_b.get();
+    auto pd_ma = d_ma.get();
 
-    auto d_a_get_ct0 = d_a.get();
-    auto d_b_get_ct1 = d_b.get();
-    auto d_ma_get_ct2 = d_ma.get();
-
-    cgh.parallel_for(
-        sycl::nd_range<3>(sycl::range<3>(threadsPerGrid.get(2), threadsPerGrid.get(1), threadsPerGrid.get(0)),
-                          sycl::range<3>(threadsPerBlock3.get(2), threadsPerBlock3.get(1), threadsPerBlock3.get(0))),
-        [=](sycl::nd_item<3> item_ct1) {
-          vectorProd(d_a_get_ct0, d_b_get_ct1, d_ma_get_ct2, NUM_VALUES, item_ct1);
-        });
-  });
-  
-  stream.submit([&](sycl::handler &cgh) {
-    auto threadsPerGrid = blocksPerGrid3 * threadsPerBlock3;
-
-    auto d_a_get_ct0 = d_a.get();
-    auto d_c_get_ct1 = d_c.get();
-    auto d_mb_get_ct2 = d_mb.get();
-
-    cgh.parallel_for(
-        sycl::nd_range<3>(sycl::range<3>(threadsPerGrid.get(2), threadsPerGrid.get(1), threadsPerGrid.get(0)),
-                          sycl::range<3>(threadsPerBlock3.get(2), threadsPerBlock3.get(1), threadsPerBlock3.get(0))),
-        [=](sycl::nd_item<3> item_ct1) {
-          vectorProd(d_a_get_ct0, d_c_get_ct1, d_mb_get_ct2, NUM_VALUES, item_ct1);
-        });
-  });
-  
-  stream.submit([&](sycl::handler &cgh) {
-    auto threadsPerGrid = blocksPerGrid3 * threadsPerBlock3;
-
-    auto d_ma_get_ct0 = d_ma.get();
-    auto d_mb_get_ct1 = d_mb.get();
-    auto d_mc_get_ct2 = d_mc.get();
-
-    cgh.parallel_for(
-        sycl::nd_range<3>(sycl::range<3>(threadsPerGrid.get(2), threadsPerGrid.get(1), threadsPerGrid.get(0)),
-                          sycl::range<3>(threadsPerBlock3.get(2), threadsPerBlock3.get(1), threadsPerBlock3.get(0))),
-        [=](sycl::nd_item<3> item_ct1) {
-          matrixMul(d_ma_get_ct0, d_mb_get_ct1, d_mc_get_ct2, NUM_VALUES, item_ct1);
-        });
+    cgh.parallel_for(sycl::nd_range<3>(threadsPerGrid3, threadsPerBlock3),
+                     [=](sycl::nd_item<3> item) { vectorProd(pd_a, pd_b, pd_ma, NUM_VALUES, item); });
   });
 
   stream.submit([&](sycl::handler &cgh) {
-    auto d_mc_get_ct0 = d_mc.get();
-    auto d_b_get_ct1 = d_b.get();
-    auto d_c_get_ct2 = d_c.get();
+    auto pd_a = d_a.get();
+    auto pd_c = d_c.get();
+    auto pd_mb = d_mb.get();
 
-    cgh.parallel_for(sycl::nd_range<3>(sycl::range<3>(1, 1, blocksPerGrid) * sycl::range<3>(1, 1, threadsPerBlock),
-                                       sycl::range<3>(1, 1, threadsPerBlock)),
-                     [=](sycl::nd_item<3> item_ct1) {
-                       matrixMulVector(d_mc_get_ct0, d_b_get_ct1, d_c_get_ct2, NUM_VALUES, item_ct1);
-                     });
+    cgh.parallel_for(sycl::nd_range<3>(threadsPerGrid3, threadsPerBlock3),
+                     [=](sycl::nd_item<3> item) { vectorProd(pd_a, pd_c, pd_mb, NUM_VALUES, item); });
+  });
+
+  stream.submit([&](sycl::handler &cgh) {
+    auto pd_ma = d_ma.get();
+    auto pd_mb = d_mb.get();
+    auto pd_mc = d_mc.get();
+
+    cgh.parallel_for(sycl::nd_range<3>(threadsPerGrid3, threadsPerBlock3),
+                     [=](sycl::nd_item<3> item) { matrixMul(pd_ma, pd_mb, pd_mc, NUM_VALUES, item); });
+  });
+
+  stream.submit([&](sycl::handler &cgh) {
+    auto pd_mc = d_mc.get();
+    auto pd_b = d_b.get();
+    auto pd_c = d_c.get();
+
+    cgh.parallel_for(sycl::nd_range<1>(threadsPerGrid, threadsPerBlock),
+                     [=](sycl::nd_item<1> item) { matrixMulVector(pd_mc, pd_b, pd_c, NUM_VALUES, item); });
   });
 
   return d_a;
