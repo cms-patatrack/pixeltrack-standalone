069110c8b310436ae455737436cd039b8a389500
Andrea Bocci <andrea.bocci@cern.ch>
2020-10-07 16:02:27 +0200

    [sycl] Further simplifications and debug output
    Add a hard-coded limit on the workgroup size: the OpenCL CPU device reports
    a maximum workgroup size of 8192, but workgroups bigger than 4096 result in
    a CL_OUT_OF_RESOURCES error.

--- src/sycltest/plugin-Test2/gpuAlgo2.cc
+++ src/sycltest/plugin-Test2/gpuAlgo2.cc
@@ -12,7 +12,7 @@ namespace gpu_algo_2 {
 
   template <typename T>
   SYCL_EXTERNAL void vectorAdd(const T *a, const T *b, T *c, int numElements, sycl::nd_item<1> item) {
-    int i = item.get_group(0) * item.get_local_range().get(0) + item.get_local_id(0);
+    int i = item.get_global_id(0);
     if (i < numElements) {
       c[i] = a[i] + b[i];
     }
@@ -20,8 +20,8 @@ namespace gpu_algo_2 {
 
   template <typename T>
   SYCL_EXTERNAL void vectorProd(const T *a, const T *b, T *c, int numElements, sycl::nd_item<3> item) {
-    int row = item.get_group(1) * item.get_local_range().get(1) + item.get_local_id(1);
-    int col = item.get_group(2) * item.get_local_range().get(2) + item.get_local_id(2);
+    int row = item.get_global_id(1);
+    int col = item.get_global_id(2);
 
     if (row < numElements && col < numElements) {
       c[row * numElements + col] = a[row] * b[col];
@@ -30,8 +30,8 @@ namespace gpu_algo_2 {
 
   template <typename T>
   SYCL_EXTERNAL void matrixMul(const T *a, const T *b, T *c, int numElements, sycl::nd_item<3> item) {
-    int row = item.get_group(1) * item.get_local_range().get(1) + item.get_local_id(1);
-    int col = item.get_group(2) * item.get_local_range().get(2) + item.get_local_id(2);
+    int row = item.get_global_id(1);
+    int col = item.get_global_id(2);
 
     if (row < numElements && col < numElements) {
       T tmp = 0;
@@ -44,7 +44,7 @@ namespace gpu_algo_2 {
 
   template <typename T>
   SYCL_EXTERNAL void matrixMulVector(const T *a, const T *b, T *c, int numElements, sycl::nd_item<1> item) {
-    int row = item.get_group(1) * item.get_local_range().get(1) + item.get_local_id(1);
+    int row = item.get_global_id(0);
 
     if (row < numElements) {
       T tmp = 0;
@@ -58,6 +58,27 @@ namespace gpu_algo_2 {
 
 using namespace gpu_algo_2;
 
+std::ostream &operator<<(std::ostream &out, sycl::range<1> range) {
+  out << '(' << range.get(0) << ')';
+  return out;
+}
+
+std::ostream &operator<<(std::ostream &out, sycl::range<2> range) {
+  out << '(' << range.get(0) << ", " << range.get(1) << ')';
+  return out;
+}
+
+std::ostream &operator<<(std::ostream &out, sycl::range<3> range) {
+  out << '(' << range.get(0) << ", " << range.get(1) << ", " << range.get(2) << ')';
+  return out;
+}
+
+template <int D>
+std::ostream &operator<<(std::ostream &out, sycl::nd_range<D> range) {
+  out << '[' << range.get_global_range() << " x " << range.get_local_range() << ']';
+  return out;
+}
+
 constexpr unsigned int sqrt2(unsigned int value) {
   unsigned int result = 1;
   while (value >= 4) {
@@ -67,13 +88,22 @@ constexpr unsigned int sqrt2(unsigned int value) {
   return result;
 }
 
+constexpr unsigned int make_blocks(unsigned int size, unsigned int block_size) {
+  return (size + block_size - 1) / block_size;
+}
+
 cms::sycltools::device::unique_ptr<float[]> gpuAlgo2(sycl::queue stream) {
+  // FIXME the OpenCL CPU device reports a maximum workgroup size of 8192,
+  // but workgroups bigger than 4096 result in a CL_OUT_OF_RESOURCES error
+  const unsigned int workgroupSizeLimit = 4096;
+
   // query the device for the maximum "block size" or workgroup size
-  const unsigned int workgroupSize = stream.get_device().get_info<sycl::info::device::max_work_group_size>();
+  const unsigned int maxWorkgroupSize = std::min<unsigned int>(
+      workgroupSizeLimit, stream.get_device().get_info<sycl::info::device::max_work_group_size>());
 
   // build a rectangular "block" or workgroup
-  const unsigned int workgroupRectB = sqrt2(workgroupSize);
-  const unsigned int workgroupRectA = workgroupSize / workgroupRectB;
+  const unsigned int workgroupRectB = sqrt2(maxWorkgroupSize);
+  const unsigned int workgroupRectA = maxWorkgroupSize / workgroupRectB;
 
   auto h_a = cms::sycltools::make_host_unique<float[]>(NUM_VALUES, stream);
   auto h_b = cms::sycltools::make_host_unique<float[]>(NUM_VALUES, stream);
@@ -89,22 +119,21 @@ cms::sycltools::device::unique_ptr<float[]> gpuAlgo2(sycl::queue stream) {
   stream.memcpy(d_a.get(), h_a.get(), NUM_VALUES * sizeof(float));
   stream.memcpy(d_b.get(), h_b.get(), NUM_VALUES * sizeof(float));
 
-  unsigned int threadsPerBlock = std::min<unsigned int>(NUM_VALUES, workgroupSize);
-  unsigned int blocksPerGrid = (NUM_VALUES + threadsPerBlock - 1) / threadsPerBlock;
-  unsigned int threadsPerGrid = threadsPerGrid;
-
-  std::cerr << "block size: " << threadsPerBlock << " / "
-            << stream.get_device().get_info<sycl::info::device::max_work_group_size>() << std::endl;
-  std::cerr << "grid size:  " << blocksPerGrid << std::endl;
+  unsigned int threadsPerBlock = std::min<unsigned int>(NUM_VALUES, maxWorkgroupSize);
+  unsigned int blocksPerGrid = make_blocks(NUM_VALUES, threadsPerBlock);
+  unsigned int threadsPerGrid = blocksPerGrid * threadsPerBlock;
+  auto grid = sycl::nd_range<1>(threadsPerGrid, threadsPerBlock);
 
   auto d_c = cms::sycltools::make_device_unique<float[]>(NUM_VALUES, stream);
+
+  std::cerr << "block size: " << threadsPerBlock << " / " << maxWorkgroupSize << std::endl;
+  std::cerr << "grid size:  " << blocksPerGrid << std::endl;
   stream.submit([&](sycl::handler &cgh) {
     auto pd_a = d_a.get();
     auto pd_b = d_b.get();
     auto pd_c = d_c.get();
 
-    cgh.parallel_for(sycl::nd_range<1>(threadsPerGrid, threadsPerBlock),
-                     [=](sycl::nd_item<1> item) { vectorAdd(pd_a, pd_b, pd_c, NUM_VALUES, item); });
+    cgh.parallel_for(grid, [=](sycl::nd_item<1> item) { vectorAdd(pd_a, pd_b, pd_c, NUM_VALUES, item); });
   });
 
   auto d_ma = cms::sycltools::make_device_unique<float[]>(NUM_VALUES * NUM_VALUES, stream);
@@ -112,44 +141,48 @@ cms::sycltools::device::unique_ptr<float[]> gpuAlgo2(sycl::queue stream) {
   auto d_mc = cms::sycltools::make_device_unique<float[]>(NUM_VALUES * NUM_VALUES, stream);
 
   sycl::range<3> threadsPerBlock3{1, workgroupRectB, workgroupRectA};
-  sycl::range<3> blocksPerGrid3{
-      1, (NUM_VALUES + workgroupRectB - 1) / workgroupRectB, (NUM_VALUES + workgroupRectA - 1) / workgroupRectA};
+  sycl::range<3> blocksPerGrid3{1, make_blocks(NUM_VALUES, workgroupRectB), make_blocks(NUM_VALUES, workgroupRectA)};
   auto threadsPerGrid3 = blocksPerGrid3 * threadsPerBlock3;
+  auto grid3 = sycl::nd_range<3>(threadsPerGrid3, threadsPerBlock3);
 
+  std::cerr << "block size: " << threadsPerBlock3 << " / " << maxWorkgroupSize << std::endl;
+  std::cerr << "grid size:  " << blocksPerGrid3 << std::endl;
   stream.submit([&](sycl::handler &cgh) {
     auto pd_a = d_a.get();
     auto pd_b = d_b.get();
     auto pd_ma = d_ma.get();
 
-    cgh.parallel_for(sycl::nd_range<3>(threadsPerGrid3, threadsPerBlock3),
-                     [=](sycl::nd_item<3> item) { vectorProd(pd_a, pd_b, pd_ma, NUM_VALUES, item); });
+    cgh.parallel_for(grid3, [=](sycl::nd_item<3> item) { vectorProd(pd_a, pd_b, pd_ma, NUM_VALUES, item); });
   });
 
+  std::cerr << "block size: " << threadsPerBlock3 << " / " << maxWorkgroupSize << std::endl;
+  std::cerr << "grid size:  " << blocksPerGrid3 << std::endl;
   stream.submit([&](sycl::handler &cgh) {
     auto pd_a = d_a.get();
     auto pd_c = d_c.get();
     auto pd_mb = d_mb.get();
 
-    cgh.parallel_for(sycl::nd_range<3>(threadsPerGrid3, threadsPerBlock3),
-                     [=](sycl::nd_item<3> item) { vectorProd(pd_a, pd_c, pd_mb, NUM_VALUES, item); });
+    cgh.parallel_for(grid3, [=](sycl::nd_item<3> item) { vectorProd(pd_a, pd_c, pd_mb, NUM_VALUES, item); });
   });
 
+  std::cerr << "block size: " << threadsPerBlock3 << " / " << maxWorkgroupSize << std::endl;
+  std::cerr << "grid size:  " << blocksPerGrid3 << std::endl;
   stream.submit([&](sycl::handler &cgh) {
     auto pd_ma = d_ma.get();
     auto pd_mb = d_mb.get();
     auto pd_mc = d_mc.get();
 
-    cgh.parallel_for(sycl::nd_range<3>(threadsPerGrid3, threadsPerBlock3),
-                     [=](sycl::nd_item<3> item) { matrixMul(pd_ma, pd_mb, pd_mc, NUM_VALUES, item); });
+    cgh.parallel_for(grid3, [=](sycl::nd_item<3> item) { matrixMul(pd_ma, pd_mb, pd_mc, NUM_VALUES, item); });
   });
 
+  std::cerr << "block size: " << threadsPerBlock << " / " << maxWorkgroupSize << std::endl;
+  std::cerr << "grid size:  " << blocksPerGrid << std::endl;
   stream.submit([&](sycl::handler &cgh) {
     auto pd_mc = d_mc.get();
     auto pd_b = d_b.get();
     auto pd_c = d_c.get();
 
-    cgh.parallel_for(sycl::nd_range<1>(threadsPerGrid, threadsPerBlock),
-                     [=](sycl::nd_item<1> item) { matrixMulVector(pd_mc, pd_b, pd_c, NUM_VALUES, item); });
+    cgh.parallel_for(grid, [=](sycl::nd_item<1> item) { matrixMulVector(pd_mc, pd_b, pd_c, NUM_VALUES, item); });
   });
 
   return d_a;
