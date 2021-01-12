07d701455a5fdd11ccfe6f7771da448bcfb9bfe2
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2021-01-12 15:41:35 +0100

    [alpaka] Add AtomicPairCounter to AlpakaCore + associated test.

---
+++ src/alpaka/AlpakaCore/AtomicPairCounter.h
@@ -0,0 +1,54 @@
+#ifndef HeterogeneousCore_CUDAUtilities_interface_AtomicPairCounter_h
+#define HeterogeneousCore_CUDAUtilities_interface_AtomicPairCounter_h
+
+#include <cstdint>
+
+#include "AlpakaCore/alpakaConfig.h"   // needed?
+
+namespace cms {
+  namespace Alpaka {
+
+    class AtomicPairCounter {
+    public:
+      using c_type = unsigned long long int;
+
+      ALPAKA_FN_HOST_ACC AtomicPairCounter() {}  //  needed?
+      ALPAKA_FN_HOST_ACC AtomicPairCounter(c_type i) { counter.ac = i; } //  needed?
+
+      ALPAKA_FN_HOST_ACC AtomicPairCounter& operator=(c_type i) {
+	counter.ac = i;
+	return *this;
+      }
+
+      struct Counters {
+        uint32_t n;  // in a "One to Many" association is the number of "One"
+        uint32_t m;  // in a "One to Many" association is the total number of associations
+      };
+
+      union Atomic2 {
+        Counters counters;
+        c_type ac;
+      };
+
+      static constexpr c_type incr = 1UL << 32;
+
+      ALPAKA_FN_HOST_ACC Counters get() const { return counter.counters; }
+
+      // increment n by 1 and m by i.  return previous value
+      template<typename T_Acc>
+	ALPAKA_FN_ACC ALPAKA_FN_INLINE Counters add(T_Acc const & acc, uint32_t i) {  // not ALPAKA_FN_HOST_ACC ?
+	c_type c = i;
+	c += incr;
+	Atomic2 ret;
+	ret.ac = alpaka::atomic::atomicOp<alpaka::atomic::op::Add>(acc, &counter.ac, c);
+	return ret.counters;
+      }
+
+    private:
+      Atomic2 counter;
+    };
+
+  }  // namespace Alpaka
+}  // namespace cms
+
+#endif  // HeterogeneousCore_CUDAUtilities_interface_AtomicPairCounter_h
