e8cbb93e728d745c71a88d6a9e8257d53cd6a7b6
Andrea Bocci <andrea.bocci@cern.ch>
2021-03-01 09:46:36 +0100

    [cudatest, cudadev] Fall back to stream-ordered memory allocations
    Propagate the cudaMallocAsync/cudaFreeAsync use to the cudatest and
    cudadev programs.

--- src/cudatest/CUDACore/allocate_device.h
+++ src/cudatest/CUDACore/allocate_device.h
@@ -6,10 +6,10 @@
 namespace cms {
   namespace cuda {
     // Allocate device memory
-    void *allocate_device(int dev, size_t nbytes, cudaStream_t stream);
+    void *allocate_device(int device, size_t nbytes, cudaStream_t stream);
 
     // Free device memory (to be called from unique_ptr)
-    void free_device(int device, void *ptr);
+    void free_device(int device, void *ptr, cudaStream_t stream);
   }  // namespace cuda
 }  // namespace cms
 
