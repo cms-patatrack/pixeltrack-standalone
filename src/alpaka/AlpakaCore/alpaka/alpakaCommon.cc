938438532a81029780cd2fc3ce1610f46bd4300b
Gabrielle Hugo <gabrielle.hugo83@gmail.com>
2021-04-19 12:03:37 +0200

    Remove aweful quick hack for host and device backends, use extern instead

---
+++ src/alpaka/AlpakaCore/alpaka/alpakaCommon.cc
@@ -0,0 +1,8 @@
+#include "AlpakaCore/alpakaCommon.h"
+
+
+namespace ALPAKA_ACCELERATOR_NAMESPACE {
+  const DevHost host = alpaka::getDevByIdx<PltfHost>(0u);
+  const DevAcc1 device = alpaka::getDevByIdx<PltfAcc1>(0u);
+}
+
--- src/alpaka/AlpakaCore/alpakaCommon.h
+++ src/alpaka/AlpakaCore/alpakaCommon.h
@@ -6,10 +6,9 @@
 //#include "AlpakaCore/alpakaWorkDivHelper.h"
 
 
-static const DevHost host = alpaka::getDevByIdx<PltfHost>(0u);
-
 namespace ALPAKA_ACCELERATOR_NAMESPACE {
-static const DevAcc1 device = alpaka::getDevByIdx<PltfAcc1>(0u);
+  extern const DevHost host;
+  extern const DevAcc1 device;
 }
 
 
