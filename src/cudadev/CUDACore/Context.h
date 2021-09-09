5e0d4226ec7c4b844ab59aa382e1b4b70639cfbe
Matti Kortelainen <matti.kortelainen@cern.ch>
2021-09-08 18:33:11 -0700

    Remove Context.h

--- src/cudadev/CUDACore/Context.h
+++
@@ -1,7 +0,0 @@
-#ifndef HeterogeneousCore_CUDACore_Context_h
-#define HeterogeneousCore_CUDACore_Context_h
-
-#include "CUDACore/AcquireContext.h"
-#include "CUDACore/ProduceContext.h"
-#include "CUDACore/AnalyzeContext.h"
-#endif
