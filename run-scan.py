7f06238ca617bbbef08d7887cadcca93253179db
Matti Kortelainen <matti.kortelainen@cern.ch>
2021-09-28 13:40:12 -0700

    Enable --runForMinutes in run-scan.py, also reorganize the arguments

--- run-scan.py
+++ run-scan.py
@@ -7,6 +7,7 @@ import time
 import socket
 import argparse
 import subprocess
+import collections
 import multiprocessing
 
 # Number of events for each application
@@ -25,6 +26,7 @@ background_events_per_thread = 30*3600*8
 
 result_re = re.compile("Processed (?P<events>\d+) events in (?P<time>\S+) seconds, throughput (?P<throughput>\S+) events/s")
 
+Measurement = collections.namedtuple("Measurement", ["events", "time", "throughput"])
 
 def printMessage(*args):
     print(time.strftime("%y-%m-%d %H:%M:%S"), *args)
@@ -34,7 +36,7 @@ def throughput(output):
         m = result_re.search(line)
         if m:
             printMessage(line.rstrip())
-            return (float(m.group("throughput")), float(m.group("time")))
+            return Measurement(int(m.group("events")), float(m.group("time")), float(m.group("throughput")))
 
     raise Exception("Did not find throughput from the log")
 
@@ -44,12 +46,12 @@ def partition_cores(cores, nth):
 
     return (cores[0:nth], cores[nth:])
 
-def run(nev, nstr, cores_main, opts, logfilename):
+def _run(processUntil, nstr, cores_main, opts, logfilename):
     nth = len(cores_main)
     with open(logfilename, "w") as logfile:
         taskset = []
         nvprof = []
-        command = [opts.program, "--maxEvents", str(nev), "--numberOfStreams", str(nstr), "--numberOfThreads", str(nth)] + opts.args
+        command = [opts.program] + processUntil + ["--numberOfStreams", str(nstr), "--numberOfThreads", str(nth)] + opts.args
         if opts.taskset:
             taskset = ["taskset", "-c", ",".join(cores_main)]
 
@@ -72,6 +74,12 @@ def run(nev, nstr, cores_main, opts, logfilename):
             raise Exception("Got return code %d, see output in the log file %s" % (p.returncode, logfilename))
     with open(logfilename) as logfile:
         return throughput(logfile)
+
+def runEvents(nev, *args, **kwargs):
+    return _run(["--maxEvents", str(nev)], *args, **kwargs)
+
+def runMinutes(mins, *args, **kwargs):
+    return _run(["--runForMinutes", str(mins)], *args, **kwargs)
 
 def launchBackground(opts, cores_bkg, logfile):
     if opts.fill <= 0:
@@ -118,7 +126,7 @@ def main(opts):
         n_streams_threads = [(s, t) for t in nthreads for s in opts.numStreams]
 
     nev_per_stream = opts.eventsPerStream
-    if nev_per_stream is None:
+    if nev_per_stream is None and opts.runForMinutes < 0:
         tmp = n_blocks_per_stream.get(os.path.basename(opts.program), None)
         if tmp is None:
             raise Exception("No default number of event blocks for program %s, and --eventsPerStream was not given" % opts.program)
@@ -154,15 +162,23 @@ def main(opts):
         if (nstr, nth) in alreadyExists:
             continue
 
-        if opts.maxStreamsToAddEvents > 0 and nstr > opts.maxStreamsToAddEvents:
-            nev = nev_per_stream * opts.maxStreamsToAddEvents
-        else:
-            nev = nev_per_stream*nstr
         (cores_main, cores_bkg) = partition_cores(cores, nth)
+
+        mins = -1
+        nev = -1
+        if opts.runForMinutes >= 0:
+            mins = opts.runForMinutes
+            run = lambda postfix: runMinutes(mins, nstr, cores_main, opts, opts.output+postfix)
+        else:
+            if opts.maxStreamsToAddEvents > 0 and nstr > opts.maxStreamsToAddEvents:
+                nev = nev_per_stream * opts.maxStreamsToAddEvents
+            else:
+                nev = nev_per_stream*nstr
+            run = lambda postfix: runEvents(nev, nstr, cores_main, opts, opts.output+postfix)
 
         if opts.warmup:
           printMessage("Warming up")
-          run(nev, nstr, cores_main, opts, opts.output+"_warmup.txt")
+          run("_warmup.txt")
           print()
           opts.warmup = False
 
@@ -175,7 +191,11 @@ def main(opts):
                 printMessage(msg)
 
             try:
-                msg = "Number of streams {} threads {} events {}".format(nstr, nth, nev)
+                msg = "Number of streams {} threads {}".format(nstr, nth)
+                if nev >= 0:
+                    msg += " events {}".format(nev)
+                else:
+                    msg += " minutes {}".format(mins)
                 if opts.taskset:
                     msg += ", running on cores " + ",".join(cores_main)
                 printMessage(msg)
@@ -184,7 +204,7 @@ def main(opts):
                     tryAgain = opts.tryAgain
                     while tryAgain > 0:
                         try:
-                            (th, wtime) = run(nev, nstr, cores_main, opts, opts.output+"_log_nstr{}_nth{}_n{}.txt".format(nstr, nth, i))
+                            measurement = run("_log_nstr{}_nth{}_n{}.txt".format(nstr, nth, i))
                             break
                         except Exception as e:
                             tryAgain -= 1
@@ -205,18 +225,18 @@ def main(opts):
 
             if opts.dryRun:
                 continue
-            throughputs.append(th)
+            throughputs.append(measurement.throughput)
             data["results"].append(dict(
                 hostname=hostname,
                 threads=nth,
                 streams=nstr,
-                events=nev,
-                throughput=th,
+                events=measurement.events,
+                throughput=measurement.throughput,
             ))
             # Save results after each test
             with open(outputJson, "w") as out:
                 json.dump(data, out, indent=2)
-            if opts.stopAfterWallTime > 0 and wtime > opts.stopAfterWallTime:
+            if opts.stopAfterWallTime > 0 and measurement.time > opts.stopAfterWallTime:
                 stop = True
                 break
 
@@ -234,36 +254,47 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run a scan of a given test program")
     parser.add_argument("program", type=str,
                         help="Path to the test program to run")
-    parser.add_argument("-o", "--output", type=str, default="result",
-                        help="Prefix of output JSON and log files. If the output JSON file exists, it will be updated (see also --overwrite) (default: 'result')")
-    parser.add_argument("--overwrite", action="store_true",
-                        help="Overwrite the output JSON instead of updating it")
-    parser.add_argument("--append", action="store_true",
-                        help="Append new (stream, threads) results insteads of ignoring already existing point")
-    parser.add_argument("--taskset", action="store_true",
-                        help="Use taskset to explicitly set the cores where to run on")
-    parser.add_argument("--tasksetCores", type=str, default="",
-                        help="Comma-separated list of cores to be used for taskset in that order. Default (empty) is to use range(0, N(cores))")
-    parser.add_argument("--fill", type=int, default=-1,
-                        help="Launch serial program in the background so that this many threads are always running. If given, this will also become the upper limit for the number of threads instead of the number of cores of the machine. (default: -1 to disable")
-    parser.add_argument("--bkgNice", type=int, default=None,
-                        help="If given, use this 'nice' level for the background program")
-    parser.add_argument("--minThreads", type=int, default=1,
-                        help="Minimum number of threads to use in the scan (default: 1)")
-    parser.add_argument("--maxThreads", type=int, default=-1,
-                        help="Maximum number of threads to use in the scan (default: -1 for the number of cores)")
-    parser.add_argument("--numThreads", type=str, default="",
-                        help="Comma separated list of numbers of threads to use in the scan (default: empty for all)")
-    parser.add_argument("--numStreams", type=str, default="",
-                        help="Comma separated list of numbers of streams to use in the scan (default: empty for always the same as the number of threads). If both number of threads and number of streams have more than 1 element, a 2D scan is done with all the combinations")
-    parser.add_argument("--eventsPerStream", type=int, default=None,
-                        help="Number of events to be used per EDM stream (default: 400*4kev for cuda, others also hardcoded in the top of the script file)")
-    parser.add_argument("--maxStreamsToAddEvents", type=int, default=-1,
-                        help="Maximum number of streams to add events (default: -1 for no limit")
-    parser.add_argument("--stopAfterWallTime", type=int, default=-1,
-                        help="Stop running after the wall time of the job reaches this many in seconds (default: -1 for no limit)")
-    parser.add_argument("--repeat", type=int, default=1,
-                        help="Repeat each point this many times (default: 1)")
+
+    output_group = parser.add_argument_group("JSON output arguments")
+    output_group.add_argument("-o", "--output", type=str, default="result",
+                              help="Prefix of output JSON and log files. If the output JSON file exists, it will be updated (see also --overwrite) (default: 'result')")
+    output_group.add_argument("--overwrite", action="store_true",
+                              help="Overwrite the output JSON instead of updating it")
+    output_group.add_argument("--append", action="store_true",
+                              help="Append new (stream, threads) results insteads of ignoring already existing point")
+
+    scan_group = parser.add_argument_group("Scan arguments")
+    scan_group.add_argument("--repeat", type=int, default=1,
+                            help="Repeat each point this many times (default: 1)")
+    scan_group.add_argument("--minThreads", type=int, default=1,
+                            help="Minimum number of threads to use in the scan (default: 1)")
+    scan_group.add_argument("--maxThreads", type=int, default=-1,
+                            help="Maximum number of threads to use in the scan (default: -1 for the number of cores)")
+    scan_group.add_argument("--numThreads", type=str, default="",
+                            help="Comma separated list of numbers of threads to use in the scan (default: empty for all)")
+    scan_group.add_argument("--numStreams", type=str, default="",
+                            help="Comma separated list of numbers of streams to use in the scan (default: empty for always the same as the number of threads). If both number of threads and number of streams have more than 1 element, a 2D scan is done with all the combinations")
+    scan_group.add_argument("--stopAfterWallTime", type=int, default=-1,
+                            help="Stop running after the wall time of the job reaches this many in seconds (default: -1 for no limit)")
+
+    nevents_group = parser.add_argument_group("Setting number of events arguments")
+    nevents_group.add_argument("--eventsPerStream", type=int, default=None,
+                               help="Number of events to be used per EDM stream (default: 400*4kev for cuda, others also hardcoded in the top of the script file)")
+    nevents_group.add_argument("--maxStreamsToAddEvents", type=int, default=-1,
+                               help="Maximum number of streams to add events (default: -1 for no limit")
+    nevents_group.add_argument("--runForMinutes", type=int, default=-1,
+                               help="Process the set of events until this many minutes has elapsed. Conflicts with --eventsPerStream and --maxStreamsToAddEvents. (default -1 for disabled)")
+
+    fill_group = parser.add_argument_group("Node filling and pinning arguments")
+    fill_group.add_argument("--taskset", action="store_true",
+                            help="Use taskset to explicitly set the cores where to run on")
+    fill_group.add_argument("--tasksetCores", type=str, default="",
+                            help="Comma-separated list of cores to be used for taskset in that order. Default (empty) is to use range(0, N(cores))")
+    fill_group.add_argument("--fill", type=int, default=-1,
+                            help="Launch serial program in the background so that this many threads are always running. If given, this will also become the upper limit for the number of threads instead of the number of cores of the machine. (default: -1 to disable")
+    fill_group.add_argument("--bkgNice", type=int, default=None,
+                            help="If given, use this 'nice' level for the background program")
+
     parser.add_argument("--tryAgain", type=int, default=1,
                         help="In case of failure on a point, try again at most this many times (default: 1)")
     parser.add_argument("--warmup", action="store_true",
@@ -286,5 +317,10 @@ if __name__ == "__main__":
         opts.tasksetCores = opts.tasksetCores.split(",")
     if len(opts.tasksetCores) > 0 and opts.fill != -1 and len(opts.tasksetCores) != opts.fill:
         parser.error("When both --tasksetCores and --fill are given, --fill must match to the number of elements in --tasksetCores. No got --fill {} and {} elements in --tasksetCores {}".format(opts.fill, len(opts.tasksetCores)))
+    if opts.runForMinutes >= 0:
+        if opts.eventsPerStream is not None:
+            parser.error("--runForMinutes and --eventsPerStream can not be used together")
+        if opts.maxStreamsToAddEvents >= 0:
+            parser.error("--runForMinutes and --maxStreamsToAddEvents can not be used together")
 
     main(opts)
