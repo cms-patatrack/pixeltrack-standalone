d1943ba523a77bd380fcfc7ab1b601aaa6bfaa0b
Matti Kortelainen <matti.kortelainen@cern.ch>
2021-10-04 06:46:19 -0700

    [cudacompat] Remove ESProduct

--- src/cudacompat/CondFormats/PixelCPEFast.h
+++ src/cudacompat/CondFormats/PixelCPEFast.h
@@ -2,9 +2,8 @@
 #define RecoLocalTracker_SiPixelRecHits_PixelCPEFast_h
 
 #include <utility>
+#include <vector>
 
-#include "CUDACore/ESProduct.h"
-#include "CUDACore/HostAllocator.h"
 #include "CondFormats/pixelCPEforGPU.h"
 
 class PixelCPEFast {
@@ -12,10 +11,6 @@ public:
   PixelCPEFast(std::string const &path);
 
   ~PixelCPEFast() = default;
-
-  // The return value can only be used safely in kernels launched on
-  // the same cudaStream, or after cudaStreamSynchronize.
-  const pixelCPEforGPU::ParamsOnGPU *getGPUProductAsync(cudaStream_t cudaStream) const;
 
   pixelCPEforGPU::ParamsOnGPU const &getCPUProduct() const { return cpuData_; }
 
@@ -28,16 +23,6 @@ private:
   pixelCPEforGPU::AverageGeometry m_averageGeometry;
 
   pixelCPEforGPU::ParamsOnGPU cpuData_;
-
-  struct GPUData {
-    ~GPUData();
-    // not needed if not used on CPU...
-    pixelCPEforGPU::ParamsOnGPU h_paramsOnGPU;
-    pixelCPEforGPU::ParamsOnGPU *d_paramsOnGPU = nullptr;  // copy of the above on the Device
-  };
-  cms::cuda::ESProduct<GPUData> gpuData_;
-
-  void fillParamsForGpu();
 };
 
 #endif  // RecoLocalTracker_SiPixelRecHits_PixelCPEFast_h
