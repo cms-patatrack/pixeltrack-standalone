c879756c430990c33c17d7b0ab5df2c5d1db1321
Matti Kortelainen <matti.kortelainen@cern.ch>
2021-10-07 15:25:28 -0500

    [cudadev] Add --runForMinutes option to process the set of events for a given amount of time

--- src/cudadev/bin/Source.h
+++ src/cudadev/bin/Source.h
@@ -2,9 +2,11 @@
 #define Source_h
 
 #include <atomic>
+#include <chrono>
 #include <filesystem>
-#include <string>
 #include <memory>
+#include <mutex>
+#include <string>
 
 #include "Framework/Event.h"
 #include "DataFormats/FEDRawDataCollection.h"
@@ -15,16 +17,31 @@
 namespace edm {
   class Source {
   public:
-    explicit Source(int maxEvents, ProductRegistry& reg, std::filesystem::path const& datadir, bool validation);
+    explicit Source(int maxEvents,
+                    int runForMinutes,
+                    ProductRegistry& reg,
+                    std::filesystem::path const& datadir,
+                    bool validation);
+
+    void startProcessing();
 
     int maxEvents() const { return maxEvents_; }
+    int processedEvents() const { return numEvents_; }
 
     // thread safe
     std::unique_ptr<Event> produce(int streamId, ProductRegistry const& reg);
 
   private:
     int maxEvents_;
-    std::atomic<int> numEvents_;
+
+    // these are all for the mode where the processing length is limited by time
+    int const runForMinutes_;
+    std::chrono::steady_clock::time_point startTime_;
+    std::mutex timeMutex_;
+    std::atomic<int> numEventsTimeLastCheck_ = 0;
+    std::atomic<bool> shouldStop_ = false;
+
+    std::atomic<int> numEvents_ = 0;
     EDPutTokenT<FEDRawDataCollection> const rawToken_;
     EDPutTokenT<DigiClusterCount> digiClusterToken_;
     EDPutTokenT<TrackCount> trackToken_;
