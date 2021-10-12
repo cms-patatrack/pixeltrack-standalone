70e791960ef425f8e66bea8915d80ea8754510e2
Matti Kortelainen <matti.kortelainen@cern.ch>
2021-10-12 11:56:19 -0700

    [kokkos] Remove runtime recursion from find_ntuplets()
    Originally by Vincenzo Innocente in https://github.com/cms-sw/cmssw/pull/35473
    Contains also https://github.com/cms-sw/cmssw/pull/35542 by Andrea Bocci

--- src/kokkos/plugin-PixelTriplets/GPUCACell.h
+++ src/kokkos/plugin-PixelTriplets/GPUCACell.h
@@ -281,6 +281,7 @@ public:
 
   // trying to free the track building process from hardcoded layers, leaving
   // the visit of the graph based on the neighborhood connections between cells.
+  template <int DEPTH>
   KOKKOS_INLINE_FUNCTION void find_ntuplets(Hits const& hh,
                                             GPUCACell* __restrict__ cells,
                                             CellTracksVector& cellTracks,
@@ -305,7 +306,7 @@ public:
       if (cells[otherCell].theDoubletId < 0)
         continue;  // killed by earlyFishbone
       last = false;
-      cells[otherCell].find_ntuplets(
+      cells[otherCell].find_ntuplets<DEPTH - 1>(
           hh, cells, cellTracks, foundNtuplets, apc, quality, tmpNtuplet, minHitsPerNtuplet, startAt0);
     }
     if (last) {  // if long enough save...
@@ -350,5 +351,19 @@ private:
   hindex_type theInnerHitId;
   hindex_type theOuterHitId;
 };
+
+template <>
+KOKKOS_INLINE_FUNCTION void GPUCACell::find_ntuplets<0>(Hits const& hh,
+                                                        GPUCACell* __restrict__ cells,
+                                                        CellTracksVector& cellTracks,
+                                                        HitContainer& foundNtuplets,
+                                                        cms::kokkos::AtomicPairCounter& apc,
+                                                        Quality* __restrict__ quality,
+                                                        TmpTuple& tmpNtuplet,
+                                                        const unsigned int minHitsPerNtuplet,
+                                                        bool startAt0) const {
+  printf("ERROR: GPUCACell::find_ntuplets reached full depth!\n");
+  assert(false);
+}
 
 #endif  // RecoPixelVertexing_PixelTriplets_plugins_GPUCACell_h
