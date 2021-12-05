f1ce04fdf98c311eff3e9610ca9ea7678522e39c
Andrea Bocci <andrea.bocci@cern.ch>
2021-12-05 19:31:35 +0100

    [alpaka] Simplify typedefs

--- src/alpaka/AlpakaCore/alpakaConfigCommon.h
+++ src/alpaka/AlpakaCore/alpakaConfigCommon.h
@@ -4,30 +4,38 @@
 #include <alpaka/alpaka.hpp>
 
 namespace alpaka_common {
+
+  // common types and dimensions
   using Idx = uint32_t;
   using Extent = uint32_t;
   using Offsets = Extent;
-  using DevHost = alpaka::DevCpu;
-  using PltfHost = alpaka::Pltf<DevHost>;
 
   using Dim1D = alpaka::DimInt<1u>;
   using Dim2D = alpaka::DimInt<2u>;
+  using Dim3D = alpaka::DimInt<3u>;
 
   template <typename TDim>
   using Vec = alpaka::Vec<TDim, Idx>;
   using Vec1D = Vec<Dim1D>;
   using Vec2D = Vec<Dim2D>;
+  using Vec3D = Vec<Dim3D>;
 
   template <typename TDim>
   using WorkDiv = alpaka::WorkDivMembers<TDim, Idx>;
   using WorkDiv1D = WorkDiv<Dim1D>;
   using WorkDiv2D = WorkDiv<Dim2D>;
+  using WorkDiv3D = WorkDiv<Dim3D>;
+
+  // host types
+  using DevHost = alpaka::DevCpu;
+  using PltfHost = alpaka::Pltf<DevHost>;
 
   template <typename TData>
   using AlpakaHostBuf = alpaka::Buf<DevHost, TData, Dim1D, Idx>;
 
   template <typename TData>
   using AlpakaHostView = alpaka::ViewPlainPtr<DevHost, TData, Dim1D, Idx>;
+
 }  // namespace alpaka_common
 
 // trick to force expanding ALPAKA_ACCELERATOR_NAMESPACE before stringification inside DEFINE_FWK_MODULE
