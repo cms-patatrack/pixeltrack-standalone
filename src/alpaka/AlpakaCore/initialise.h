c564534b2abb7f375b2cf8cce386b0a9f4d1093c
Andrea Bocci <andrea.bocci@cern.ch>
2021-12-14 11:49:11 +0100

    [alpaka] Initialise only the selected backends (#286)
    Only the backends enabled at build time are supported in the command line
    options, and listed in the help message.
    
    Only the backends selected on the command line are initialised.

--- src/alpaka/AlpakaCore/initialise.h
+++ src/alpaka/AlpakaCore/initialise.h
@@ -1,6 +1,24 @@
 #ifndef INITIALIZE_H
 #define INITIALIZE_H
 
+#ifdef ALPAKA_ACC_CPU_B_SEQ_T_SEQ_SUPPORTED
+namespace alpaka_serial_sync {
+  void initialise();
+}
+#endif  // ALPAKA_ACC_CPU_B_SEQ_T_SEQ_SUPPORTED
+
+#ifdef ALPAKA_ACC_CPU_B_TBB_T_SEQ_SUPPORTED
+namespace alpaka_tbb_async {
+  void initialise();
+}
+#endif  // ALPAKA_ACC_CPU_B_TBB_T_SEQ_SUPPORTED
+
+#ifdef ALPAKA_ACC_GPU_CUDA_SUPPORTED
+namespace alpaka_cuda_async {
+  void initialise();
+}
+#endif  // ALPAKA_ACC_GPU_CUDA_SUPPORTED
+
 void initialise();
 
 #endif  // INITIALIZE_H
