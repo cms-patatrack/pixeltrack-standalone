14aa2a3bbe9de86f064f161a1b4340ce12dea80d
Andrea Bocci <andrea.bocci@cern.ch>
2022-01-23 09:54:17 +0100

    [alpaka] Make the initialisaiton more verbose

--- src/alpaka/AlpakaCore/initialise.cc
+++ src/alpaka/AlpakaCore/initialise.cc
@@ -3,6 +3,11 @@
 #include "AlpakaCore/backend.h"
 #include "AlpakaCore/initialise.h"
 
+static constexpr const char* suffix(size_t size) {
+  constexpr const char* devices[] = { "devices.", "device:", "devices:" };
+  return devices[size < 2 ? size : 2];
+}
+
 namespace cms::alpakatools {
 
 #ifdef ALPAKA_ACC_CPU_B_SEQ_T_SEQ_ENABLED
@@ -11,9 +16,14 @@ namespace cms::alpakatools {
     using Platform = alpaka_serial_sync::Platform;
     if (devices<Platform>.empty()) {
       devices<Platform> = enumerate<Platform>();
-      std::cout << "platform initialised" << std::endl;
+      auto size = devices<Platform>.size();
+      std::cout << "CPU platform succesfully initialised" << std::endl;
+      std::cout << "Found " << size << " " << suffix(size) << std::endl;
+      for (auto const& device: devices<Platform>) {
+        std::cout << "  - " << alpaka::getName(device) << std::endl;
+      }
     } else {
-      std::cout << "platform already initialised" << std::endl;
+      std::cout << "CPU platform already initialised" << std::endl;
     }
   }
 #endif  // ALPAKA_ACC_CPU_B_SEQ_T_SEQ_ENABLED
@@ -24,9 +34,14 @@ namespace cms::alpakatools {
     using Platform = alpaka_tbb_async::Platform;
     if (devices<Platform>.empty()) {
       devices<Platform> = enumerate<Platform>();
-      std::cout << "platform initialised" << std::endl;
+      auto size = devices<Platform>.size();
+      std::cout << "CPU platform succesfully initialised" << std::endl;
+      std::cout << "Found " << size << " " << suffix(size) << std::endl;
+      for (auto const& device: devices<Platform>) {
+        std::cout << "  - " << alpaka::getName(device) << std::endl;
+      }
     } else {
-      std::cout << "platform already initialised" << std::endl;
+      std::cout << "CPU platform already initialised" << std::endl;
     }
   }
 #endif  // ALPAKA_ACC_CPU_B_TBB_T_SEQ_ENABLED
@@ -37,9 +52,14 @@ namespace cms::alpakatools {
     using Platform = alpaka_cuda_async::Platform;
     if (devices<Platform>.empty()) {
       devices<Platform> = enumerate<Platform>();
-      std::cout << "platform initialised" << std::endl;
+      auto size = devices<Platform>.size();
+      std::cout << "CUDA platform succesfully initialised" << std::endl;
+      std::cout << "Found " << size << " " << suffix(size) << std::endl;
+      for (auto const& device: devices<Platform>) {
+        std::cout << "  - " << alpaka::getName(device) << std::endl;
+      }
     } else {
-      std::cout << "platform already initialised" << std::endl;
+      std::cout << "CUDA platform already initialised" << std::endl;
     }
   }
 #endif  // ALPAKA_ACC_GPU_CUDA_ENABLED
