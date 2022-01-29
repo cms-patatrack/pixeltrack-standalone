67d34f97a8d2528134c7f6fe7bea78f4944b069d
Andrea Bocci <andrea.bocci@cern.ch>
2022-01-29 16:24:07 +0100

    [alpaka] Reduce the verbosity of the initialisation message

--- src/alpaka/AlpakaCore/initialise.h
+++ src/alpaka/AlpakaCore/initialise.h
@@ -17,13 +17,13 @@ namespace cms::alpakatools {
     if (devices<TPlatform>.empty()) {
       devices<TPlatform> = enumerate<TPlatform>();
       auto size = devices<TPlatform>.size();
-      std::cout << edm::demangle<TPlatform> << " platform succesfully initialised." << std::endl;
+      //std::cout << edm::demangle<TPlatform> << " platform succesfully initialised." << std::endl;
       std::cout << "Found " << size << " " << suffix[size < 2 ? size : 2] << std::endl;
       for (auto const& device: devices<TPlatform>) {
         std::cout << "  - " << alpaka::getName(device) << std::endl;
       }
     } else {
-      std::cout << edm::demangle<TPlatform> << " platform already initialised." << std::endl;
+      //std::cout << edm::demangle<TPlatform> << " platform already initialised." << std::endl;
     }
   }
 
