50570e81578223df5359dfde9183b5fb67fe64c4
Andrea Bocci <andrea.bocci@cern.ch>
2022-04-14 14:52:03 +0200

    [alpaka] Static splitting of event streams across multiple backends
    Each event stream is associated to a different backend, according to the
    optional weight specified on the command line.

--- src/alpaka/AlpakaCore/backend.h
+++ src/alpaka/AlpakaCore/backend.h
@@ -3,4 +3,15 @@
 
 enum class Backend { SERIAL, TBB, CUDA, HIP };
 
+inline std::string const& name(Backend backend) {
+  static const std::string names[] = {"serial_sync", "tbb_async", "cuda_async", "rocm_async"};
+  return names[static_cast<int>(backend)];
+}
+
+template <typename T>
+inline T& operator<<(T& out, Backend backend) {
+  out << name(backend);
+  return out;
+}
+
 #endif  // AlpakaCore_backend_h
