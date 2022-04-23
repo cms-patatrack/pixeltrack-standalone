c964e476529854548b98d89b30f598672b49a9bc
SimonB00 <simone.balducci00@gmail.com>
2022-04-23 11:54:56 +0200

    Creating hits tokens (1)

--- src/serial/plugin-PixelTriplets/newFile.cc
+++ src/serial/plugin-PixelTriplets/newFile.cc
@@ -8,6 +8,9 @@
 #include "CUDADataFormats/PixelTrackHeterogeneous.h"
 #include "CUDADataFormats/TrackingRecHit2DHeterogeneous.h"
 
+// I take a generic file number, just for reference
+int test_file = 0;
+
 class myClass : public edm::EDProducer {
 public:
   explicit myClass(edm::ProductRegistry& reg);
@@ -15,20 +18,23 @@ public:
   
 private:
   void produce(edm::Event& iEvent, const edm::EventSetup& iSetup) override;
-  //edm::EDGetTokenT<TrackingRecHit2DCPU> tokenHitCPU_;
+  edm::EDGetTokenT<TrackingRecHit2DCPU> tokenHitCPU_;
 
+  TrackingRecHit2DCPU PixelRecHitGPUKernel algo_;
   CAHitNtupletGeneratorOnGPU gpuAlgo_;
   edm::EDPutTokenT<std::vector<float>> test_Token;
 };
 
 myClass::myClass(edm::ProductRegistry& reg)
     : gpuAlgo_(reg),
-     test_Token(reg.produces<std::vector<float>>()) {}
+      test_Token(reg.produces<std::vector<float>>()),
+      tokenHitCPU_(reg.produces<TrackingRecHit2DCPU>()) {}
 
 void myClass::produce(edm::Event& iEvent, const edm::EventSetup& es) {
   std::cout << "I'm here!" << '\n';
   std::vector<float> test = {7,6,5,4,3,2,1};
   iEvent.emplace(test_Token, test);
+  iEvent.emplace(tokenHitCPU_, algo_.makeHits(test_file));
 }
 
 DEFINE_FWK_MODULE(myClass);
