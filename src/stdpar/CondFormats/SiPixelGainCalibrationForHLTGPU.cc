98099c8ea62a7b1a63dde9e622be72519ebda500
Julien Esseiva <esseivaju@gmail.com>
2022-07-27 16:04:52 -0700

    [stdpar] Updated constructor to take a vector of DecodingStructure instead of char. Allocate data to heap instead of unified memory

--- src/stdpar/CondFormats/SiPixelGainCalibrationForHLTGPU.cc
+++ src/stdpar/CondFormats/SiPixelGainCalibrationForHLTGPU.cc
@@ -1,7 +1,5 @@
 #include <cstring>
 
-#include <cuda.h>
-
 #include "CondFormats/SiPixelGainCalibrationForHLTGPU.h"
 #include "CondFormats/SiPixelGainForHLTonGPU.h"
 #include "CUDACore/cudaCheck.h"
@@ -9,29 +7,15 @@
 #include "CUDACore/ScopedSetDevice.h"
 #include "CUDACore/StreamCache.h"
 
-SiPixelGainCalibrationForHLTGPU::SiPixelGainCalibrationForHLTGPU(SiPixelGainForHLTonGPU const& gain,
-                                                                 std::vector<char> const& gainData) {
-  cudaCheck(cudaMallocManaged(&gainForHLT_, sizeof(SiPixelGainForHLTonGPU)));
-  *gainForHLT_ = gain;
-  cudaCheck(cudaMallocManaged(&gainData_, gainData.size()));
+SiPixelGainCalibrationForHLTGPU::SiPixelGainCalibrationForHLTGPU(
+    SiPixelGainForHLTonGPU const& gain, std::vector<DecodingStructure> const& gainData) {
+  gainForHLT_ = new SiPixelGainForHLTonGPU(gain);
+  gainData_ = new DecodingStructure[gainData.size()];
   gainForHLT_->v_pedestals = gainData_;
-
   std::memcpy(gainData_, gainData.data(), gainData.size());
-  for (int device = 0, ndev = cms::cuda::deviceCount(); device < ndev; ++device) {
-#ifndef CUDAUVM_DISABLE_ADVISE
-    cudaCheck(cudaMemAdvise(gainForHLT_, sizeof(SiPixelGainForHLTonGPU), cudaMemAdviseSetReadMostly, device));
-    cudaCheck(cudaMemAdvise(gainData_, gainData.size(), cudaMemAdviseSetReadMostly, device));
-#endif
-#ifndef CUDAUVM_DISABLE_PREFETCH
-    cms::cuda::ScopedSetDevice guard{device};
-    auto stream = cms::cuda::getStreamCache().get();
-    cudaCheck(cudaMemPrefetchAsync(gainForHLT_, sizeof(SiPixelGainForHLTonGPU), device, stream.get()));
-    cudaCheck(cudaMemPrefetchAsync(gainData_, gainData.size(), device, stream.get()));
-#endif
-  }
 }
 
 SiPixelGainCalibrationForHLTGPU::~SiPixelGainCalibrationForHLTGPU() {
-  cudaCheck(cudaFree(gainForHLT_));
-  cudaCheck(cudaFree(gainData_));
+  delete gainForHLT_;
+  delete[] gainData_;
 }
--- src/stdpar/CondFormats/SiPixelGainCalibrationForHLTGPU.h
+++ src/stdpar/CondFormats/SiPixelGainCalibrationForHLTGPU.h
@@ -7,15 +7,18 @@ class SiPixelGainForHLTonGPU;
 struct SiPixelGainForHLTonGPU_DecodingStructure;
 
 class SiPixelGainCalibrationForHLTGPU {
+  using DecodingStructure = SiPixelGainForHLTonGPU_DecodingStructure;
+
 public:
-  explicit SiPixelGainCalibrationForHLTGPU(SiPixelGainForHLTonGPU const& gain, std::vector<char> const& gainData);
+  explicit SiPixelGainCalibrationForHLTGPU(SiPixelGainForHLTonGPU const& gain,
+                                           std::vector<DecodingStructure> const& gainData);
   ~SiPixelGainCalibrationForHLTGPU();
 
   const SiPixelGainForHLTonGPU* get() const { return gainForHLT_; }
 
 private:
   SiPixelGainForHLTonGPU* gainForHLT_ = nullptr;
-  SiPixelGainForHLTonGPU_DecodingStructure* gainData_ = nullptr;
+  DecodingStructure* gainData_ = nullptr;
 };
 
 #endif  // CalibTracker_SiPixelESProducers_interface_SiPixelGainCalibrationForHLTGPU_h
