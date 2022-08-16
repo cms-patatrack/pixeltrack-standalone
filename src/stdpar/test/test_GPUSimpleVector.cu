35cd8090695604d437779fc61bd4eadba0eca3c7
Julien Esseiva <esseivaju@gmail.com>
2022-08-16 16:43:04 -0700

    [stdpar] Updated SimpleVector test to use memory allocated through unique_ptr

--- src/stdpar/test/test_GPUSimpleVector.cu
+++ src/stdpar/test/test_GPUSimpleVector.cu
@@ -2,6 +2,7 @@
 #include <cassert>
 #include <iostream>
 #include <new>
+#include <memory>
 
 #include <cuda.h>
 #include <cuda_runtime.h>
@@ -24,58 +25,37 @@ __global__ void vector_emplace_back(cms::cuda::SimpleVector<int> *foo) {
 int main() {
 
   auto maxN = 10000;
-  cms::cuda::SimpleVector<int> *obj_ptr = nullptr;
-  cms::cuda::SimpleVector<int> *d_obj_ptr = nullptr;
-  cms::cuda::SimpleVector<int> *tmp_obj_ptr = nullptr;
-  int *data_ptr = nullptr;
-  int *d_data_ptr = nullptr;
 
-  cudaCheck(cudaMallocHost(&obj_ptr, sizeof(cms::cuda::SimpleVector<int>)));
-  cudaCheck(cudaMallocHost(&data_ptr, maxN * sizeof(int)));
-  cudaCheck(cudaMalloc(&d_data_ptr, maxN * sizeof(int)));
+  auto obj_unique_ptr = std::make_unique<cms::cuda::SimpleVector<int>>();
+  auto data_unique_ptr = std::make_unique<int[]>(maxN);
+  auto obj_ptr = obj_unique_ptr.get();
+  auto data_ptr = data_unique_ptr.get();
+
 
   auto v = cms::cuda::make_SimpleVector(obj_ptr, maxN, data_ptr);
 
-  cudaCheck(cudaMallocHost(&tmp_obj_ptr, sizeof(cms::cuda::SimpleVector<int>)));
-  cms::cuda::make_SimpleVector(tmp_obj_ptr, maxN, d_data_ptr);
-  assert(tmp_obj_ptr->size() == 0);
-  assert(tmp_obj_ptr->capacity() == static_cast<int>(maxN));
-
-  cudaCheck(cudaMalloc(&d_obj_ptr, sizeof(cms::cuda::SimpleVector<int>)));
-  // ... and copy the object to the device.
-  cudaCheck(cudaMemcpy(d_obj_ptr, tmp_obj_ptr, sizeof(cms::cuda::SimpleVector<int>), cudaMemcpyDefault));
+  assert(obj_ptr->size() == 0);
+  assert(obj_ptr->capacity() == static_cast<int>(maxN));
 
   int numBlocks = 5;
   int numThreadsPerBlock = 256;
-  vector_pushback<<<numBlocks, numThreadsPerBlock>>>(d_obj_ptr);
+  vector_pushback<<<numBlocks, numThreadsPerBlock>>>(obj_ptr);
   cudaCheck(cudaGetLastError());
   cudaCheck(cudaDeviceSynchronize());
-
-  cudaCheck(cudaMemcpy(obj_ptr, d_obj_ptr, sizeof(cms::cuda::SimpleVector<int>), cudaMemcpyDefault));
 
   assert(obj_ptr->size() == (numBlocks * numThreadsPerBlock < maxN ? numBlocks * numThreadsPerBlock : maxN));
-  vector_reset<<<numBlocks, numThreadsPerBlock>>>(d_obj_ptr);
+  vector_reset<<<numBlocks, numThreadsPerBlock>>>(obj_ptr);
   cudaCheck(cudaGetLastError());
   cudaCheck(cudaDeviceSynchronize());
-
-  cudaCheck(cudaMemcpy(obj_ptr, d_obj_ptr, sizeof(cms::cuda::SimpleVector<int>), cudaMemcpyDefault));
 
   assert(obj_ptr->size() == 0);
 
-  vector_emplace_back<<<numBlocks, numThreadsPerBlock>>>(d_obj_ptr);
+  vector_emplace_back<<<numBlocks, numThreadsPerBlock>>>(obj_ptr);
   cudaCheck(cudaGetLastError());
   cudaCheck(cudaDeviceSynchronize());
 
-  cudaCheck(cudaMemcpy(obj_ptr, d_obj_ptr, sizeof(cms::cuda::SimpleVector<int>), cudaMemcpyDefault));
-
   assert(obj_ptr->size() == (numBlocks * numThreadsPerBlock < maxN ? numBlocks * numThreadsPerBlock : maxN));
 
-  cudaCheck(cudaMemcpy(data_ptr, d_data_ptr, obj_ptr->size() * sizeof(int), cudaMemcpyDefault));
-  cudaCheck(cudaFreeHost(obj_ptr));
-  cudaCheck(cudaFreeHost(data_ptr));
-  cudaCheck(cudaFreeHost(tmp_obj_ptr));
-  cudaCheck(cudaFree(d_data_ptr));
-  cudaCheck(cudaFree(d_obj_ptr));
   std::cout << "TEST PASSED" << std::endl;
   return 0;
 }
