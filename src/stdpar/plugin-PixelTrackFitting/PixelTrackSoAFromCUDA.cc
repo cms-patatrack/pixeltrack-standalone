95ebfdb7b79dc5e1ebd3182b6ac81d8a5c9e521e
Julien Esseiva <esseivaju@gmail.com>
2022-08-15 22:20:13 -0700

    [stdpar] Refactor PixelTrackFitting to no longer use Product, ScopedContext and ExternalWork

--- src/stdpar/plugin-PixelTrackFitting/PixelTrackSoAFromCUDA.cc
+++ src/stdpar/plugin-PixelTrackFitting/PixelTrackSoAFromCUDA.cc
@@ -1,42 +1,23 @@
-#include <cuda_runtime.h>
-
-#include "CUDACore/Product.h"
 #include "CUDADataFormats/PixelTrack.h"
 #include "Framework/EventSetup.h"
 #include "Framework/Event.h"
 #include "Framework/PluginFactory.h"
 #include "Framework/EDProducer.h"
-#include "CUDACore/ScopedContext.h"
 
-class PixelTrackSoAFromCUDA : public edm::EDProducerExternalWork {
+class PixelTrackSoAFromCUDA : public edm::EDProducer {
 public:
   explicit PixelTrackSoAFromCUDA(edm::ProductRegistry& reg);
   ~PixelTrackSoAFromCUDA() override = default;
 
 private:
-  void acquire(edm::Event const& iEvent,
-               edm::EventSetup const& iSetup,
-               edm::WaitingTaskWithArenaHolder waitingTaskHolder) override;
   void produce(edm::Event& iEvent, edm::EventSetup const& iSetup) override;
 
-  edm::EDGetTokenT<cms::cuda::Product<PixelTrack>> tokenCUDA_;
+  edm::EDGetTokenT<PixelTrack> tokenCUDA_;
   edm::EDPutTokenT<PixelTrack> tokenSOA_;
-
-  const pixelTrack::TrackSoA* m_soa;
 };
 
 PixelTrackSoAFromCUDA::PixelTrackSoAFromCUDA(edm::ProductRegistry& reg)
-    : tokenCUDA_(reg.consumes<cms::cuda::Product<PixelTrack>>()), tokenSOA_(reg.produces<PixelTrack>()) {}
-
-void PixelTrackSoAFromCUDA::acquire(edm::Event const& iEvent,
-                                    edm::EventSetup const& iSetup,
-                                    edm::WaitingTaskWithArenaHolder waitingTaskHolder) {
-  cms::cuda::Product<PixelTrack> const& inputDataWrapped = iEvent.get(tokenCUDA_);
-  cms::cuda::ScopedContextAcquire ctx{inputDataWrapped, std::move(waitingTaskHolder)};
-  auto const& inputData = ctx.get(inputDataWrapped);
-
-  m_soa = inputData.get();
-}
+    : tokenCUDA_(reg.consumes<PixelTrack>()), tokenSOA_(reg.produces<PixelTrack>()) {}
 
 void PixelTrackSoAFromCUDA::produce(edm::Event& iEvent, edm::EventSetup const& iSetup) {
   /*
@@ -53,9 +34,10 @@ void PixelTrackSoAFromCUDA::produce(edm::Event& iEvent, edm::EventSetup const& i
   }
   std::cout << "found " << nt << " tracks in cpu SoA at " << &tsoa << std::endl;
   */
-
-  iEvent.emplace(tokenSOA_, std::make_unique<pixelTrack::TrackSoA>(*m_soa));
-  m_soa = nullptr;
+  // we have a reference to the unique_ptr owning TrackSoA
+  PixelTrack const& track_soa = iEvent.get(tokenCUDA_);
+  //Construct a new unique_ptr
+  iEvent.emplace(tokenSOA_, std::make_unique<pixelTrack::TrackSoA>(*track_soa));
 }
 
 DEFINE_FWK_MODULE(PixelTrackSoAFromCUDA);
