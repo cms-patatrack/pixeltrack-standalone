058baace8abe309021617c834b35fbf2c1f4cabc
Julien Esseiva <esseivaju@gmail.com>
2023-02-09 13:51:33 -0800

    [stdpar] Port NrokenLineFit kernels

--- src/stdpar/plugin-PixelTriplets/BrokenLineFitOnGPU.cu
+++ src/stdpar/plugin-PixelTriplets/BrokenLineFitOnGPU.cu
@@ -5,9 +5,6 @@
 void HelixFitOnGPU::launchBrokenLineKernels(HitsView const *hv, uint32_t hitsInFit, uint32_t maxNumberOfTuples) {
   assert(tuples_d);
 
-  auto blockSize = 64;
-  auto numberOfBlocks = (maxNumberOfConcurrentFits_ + blockSize - 1) / blockSize;
-
   //  Fit internals
   auto hitsGPU_ = std::make_unique<double[]>(maxNumberOfConcurrentFits_ * sizeof(Rfit::Matrix3xNd<4>) / sizeof(double));
   auto hits_geGPU_ = std::make_unique<float[]>(maxNumberOfConcurrentFits_ * sizeof(Rfit::Matrix6x4f) / sizeof(float));
@@ -16,65 +13,57 @@ void HelixFitOnGPU::launchBrokenLineKernels(HitsView const *hv, uint32_t hitsInF
 
   for (uint32_t offset = 0; offset < maxNumberOfTuples; offset += maxNumberOfConcurrentFits_) {
     // fit triplets
-    kernelBLFastFit<3><<<numberOfBlocks, blockSize, 0>>>(
+    kernelBLFastFit<3>(
         tuples_d, tupleMultiplicity_d, hv, hitsGPU_.get(), hits_geGPU_.get(), fast_fit_resultsGPU_.get(), 3, offset);
-    cudaCheck(cudaGetLastError());
 
-    kernelBLFit<3><<<numberOfBlocks, blockSize, 0>>>(tupleMultiplicity_d,
-                                                     bField_,
-                                                     outputSoa_d,
-                                                     hitsGPU_.get(),
-                                                     hits_geGPU_.get(),
-                                                     fast_fit_resultsGPU_.get(),
-                                                     3,
-                                                     offset);
-    cudaCheck(cudaGetLastError());
+    kernelBLFit<3>(tupleMultiplicity_d,
+                   bField_,
+                   outputSoa_d,
+                   hitsGPU_.get(),
+                   hits_geGPU_.get(),
+                   fast_fit_resultsGPU_.get(),
+                   3,
+                   offset);
 
     // fit quads
-    kernelBLFastFit<4><<<numberOfBlocks / 4, blockSize, 0>>>(
+    kernelBLFastFit<4>(
         tuples_d, tupleMultiplicity_d, hv, hitsGPU_.get(), hits_geGPU_.get(), fast_fit_resultsGPU_.get(), 4, offset);
-    cudaCheck(cudaGetLastError());
 
-    kernelBLFit<4><<<numberOfBlocks / 4, blockSize, 0>>>(tupleMultiplicity_d,
-                                                         bField_,
-                                                         outputSoa_d,
-                                                         hitsGPU_.get(),
-                                                         hits_geGPU_.get(),
-                                                         fast_fit_resultsGPU_.get(),
-                                                         4,
-                                                         offset);
-    cudaCheck(cudaGetLastError());
+    kernelBLFit<4>(tupleMultiplicity_d,
+                   bField_,
+                   outputSoa_d,
+                   hitsGPU_.get(),
+                   hits_geGPU_.get(),
+                   fast_fit_resultsGPU_.get(),
+                   4,
+                   offset);
 
     if (fit5as4_) {
       // fit penta (only first 4)
-      kernelBLFastFit<4><<<numberOfBlocks / 4, blockSize, 0>>>(
+      kernelBLFastFit<4>(
           tuples_d, tupleMultiplicity_d, hv, hitsGPU_.get(), hits_geGPU_.get(), fast_fit_resultsGPU_.get(), 5, offset);
-      cudaCheck(cudaGetLastError());
 
-      kernelBLFit<4><<<numberOfBlocks / 4, blockSize, 0>>>(tupleMultiplicity_d,
-                                                           bField_,
-                                                           outputSoa_d,
-                                                           hitsGPU_.get(),
-                                                           hits_geGPU_.get(),
-                                                           fast_fit_resultsGPU_.get(),
-                                                           5,
-                                                           offset);
-      cudaCheck(cudaGetLastError());
+      kernelBLFit<4>(tupleMultiplicity_d,
+                     bField_,
+                     outputSoa_d,
+                     hitsGPU_.get(),
+                     hits_geGPU_.get(),
+                     fast_fit_resultsGPU_.get(),
+                     5,
+                     offset);
     } else {
       // fit penta (all 5)
-      kernelBLFastFit<5><<<numberOfBlocks / 4, blockSize, 0>>>(
+      kernelBLFastFit<5>(
           tuples_d, tupleMultiplicity_d, hv, hitsGPU_.get(), hits_geGPU_.get(), fast_fit_resultsGPU_.get(), 5, offset);
-      cudaCheck(cudaGetLastError());
 
-      kernelBLFit<5><<<numberOfBlocks / 4, blockSize, 0>>>(tupleMultiplicity_d,
-                                                           bField_,
-                                                           outputSoa_d,
-                                                           hitsGPU_.get(),
-                                                           hits_geGPU_.get(),
-                                                           fast_fit_resultsGPU_.get(),
-                                                           5,
-                                                           offset);
-      cudaCheck(cudaGetLastError());
+      kernelBLFit<5>(tupleMultiplicity_d,
+                     bField_,
+                     outputSoa_d,
+                     hitsGPU_.get(),
+                     hits_geGPU_.get(),
+                     fast_fit_resultsGPU_.get(),
+                     5,
+                     offset);
     }
 
   }  // loop on concurrent fits
