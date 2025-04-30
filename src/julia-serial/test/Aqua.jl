e2c61f35100552416d05f4a91a6c634e75d63e0a
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2025-04-30 14:36:46 +0200

    check and fix missing compat

--- src/julia-serial/test/Aqua.jl
+++ src/julia-serial/test/Aqua.jl
@@ -5,4 +5,5 @@ using Patatrack
 @testset "Aqua.jl" begin
     Aqua.test_stale_deps(Patatrack; ignore = [:ArgParse, :BenchmarkTools, # used in main
                              :Distributions]) # used in test/
+    Aqua.test_deps_compat(Patatrack)                    
 end
